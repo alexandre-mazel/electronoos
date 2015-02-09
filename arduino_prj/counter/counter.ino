a6d1862cb97831ef218640980f6428ec12f8dd6b
alexandre-mazel <alexandre.zelma@gmail.com>
2015-02-09 22:16:07 +0100

    up

---
+++ arduino_prj/counter/counter.ino
@@ -0,0 +1,135 @@
+#include "Ai_WS2811.h"
+
+#define NUM_PIXELS 8
+#define DATA_PIN 53
+
+#define PHOTO_PIN 8
+
+Ai_WS2811 ws2811;
+
+struct CRGB {
+  unsigned char g;
+  unsigned char r;
+  unsigned char b;
+} *leds;
+
+void setVumeter( int nValue )
+{
+  // light the vumeter
+  // -nValue: [0..10000]
+  const int nNbrValuePerLed = 10000/NUM_PIXELS;
+  int nNbrLedToLighten = nValue / nNbrValuePerLed;
+  int nNbrRemaining = nValue - (nNbrLedToLighten * nNbrValuePerLed);
+//  Serial.print( "nNbrValuePerLed: " );
+//  Serial.print( nNbrValuePerLed, DEC );
+//  Serial.print( "nNbrLedToLighten: " );
+//  Serial.print( nNbrLedToLighten, DEC );
+//  Serial.print( "nNbrRemaining: " );
+//  Serial.println( nNbrRemaining, DEC );
+  
+  nNbrRemaining = map( nNbrRemaining, 0, nNbrValuePerLed, 0, 255 );
+  int i;
+  for( i = 0; i < nNbrLedToLighten; ++i )
+  {
+    leds[i].r = 255;
+    leds[i].g = 255;
+    leds[i].b = 255;    
+  }
+  if( i < NUM_PIXELS )
+  {
+    leds[i].r = nNbrRemaining;
+    leds[i].g = nNbrRemaining;
+    leds[i].b = nNbrRemaining;
+    ++i;
+  }
+  for(; i < NUM_PIXELS; ++i )
+  {
+    leds[i].r = 0;
+    leds[i].g = 0;
+    leds[i].b = 0;    
+  }
+  ws2811.sendLedData();
+}
+
+//some initial values
+void setup()
+{
+  ws2811.init(DATA_PIN,NUM_PIXELS);
+  leds = (struct CRGB*)ws2811.getRGBData();
+  Serial.begin(9600);
+  setVumeter( 10000 );
+}
+
+int nCpt = 0;
+int bPrevLighten = false;
+int nCptHidden = 0;
+void loop()
+{
+  int nLight = analogRead(  PHOTO_PIN );
+  int bLighten = nLight > 400; 
+  
+  if( bPrevLighten != bLighten )
+  {
+    if( bLighten )
+    {
+//      Serial.println( nLight, DEC );
+    }
+    else
+    {
+      ++nCptHidden;
+      Serial.println( nCptHidden, DEC );      
+    }
+    bPrevLighten = bLighten;
+  }
+  nLight = map( nLight, 0, 900,10000, 0);
+  ++nCpt;
+}
+
+/**
+ * HVS to RGB comversion (simplified to the range 0-255)
+ **/
+void setHue(int h, uint8_t n) {
+  //this is the algorithm to convert from RGB to HSV
+  double r=0; 
+  double g=0; 
+  double b=0;
+
+  double hf=h/42.5; // Not /60 as range is _not_ 0-360
+
+  int i=(int)floor(h/42.5);
+  double f = h/42.5 - i;
+  double qv = 1 - f;
+  double tv = f;
+
+  switch (i)
+  {
+  case 0: 
+    r = 1;
+    g = tv;
+    break;
+  case 1: 
+    r = qv;
+    g = 1;
+    break;
+  case 2: 
+    g = 1;
+    b = tv;
+    break;
+  case 3: 
+    g = qv;
+    b = 1;
+    break;
+  case 4:
+    r = tv;
+    b = 1;
+    break;
+  case 5: 
+    r = 1;
+    b = qv;
+    break;
+  }
+
+  leds[n].r = constrain((int)255*r,0,255);
+  leds[n].g = constrain((int)255*g,0,255);
+  leds[n].b = constrain((int)255*b,0,255);
+}

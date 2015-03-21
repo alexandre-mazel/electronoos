29ae2003fe2c2423545fa2d0a55e92dff7d7c7fb
alexandre-mazel <alexandre.zelma@gmail.com>
2015-03-22 00:12:01 +0100

    font generator

--- generate_font/generate_font.py
+++ generate_font/generate_font.py
@@ -3,16 +3,30 @@ import cv2
 def generateFontRom():
     strName = "Anonymous.png"
     #strName = "lucidagrande.jpg"
+    strName = "tut4-2.png"
     im = cv2.imread( "data/" + strName)
     
-    ox = 0; # origin
-    oy = 3;
-    sx=26; # size letter
-    sy=32;
-    ex = 1; # size interletter
-    ey = 24;
+    if( strName == "Anonymous.png" ):
+        ox = 0; # origin
+        oy = 3;
+        sx=26; # size letter
+        sy=32;
+        ex = 1; # size interletter
+        ey = 24;
+
+    if( strName == "tut4-2.png" ):
+        im = cv2.pyrDown( im ); # PyrDown only divide by 2
+        ox = 0; # origin
+        oy = 0;
+        sx=16; # size letter
+        sy=16;
+        ex = 0; # size interletter
+        ey = 0;
+        
+        sx/=2;
+        sy/=2;
     
-    for j in range(5):
+    for j in range(16):
         for i in range(16):
             x = ox+i*(sx+ex);
             y = oy+j*(sy+ey);

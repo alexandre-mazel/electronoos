93bdb099ff46e6f301eb0b5f2d2ba2284986a069
alexandre-mazel <alexandre.zelma@gmail.com>
2015-05-08 16:10:04 +0200

    new add rgb handling

---
+++ arduino_prj/glasses_receiver/Ai_WS2811.cpp
@@ -0,0 +1,193 @@
+#include "Ai_WS2811.h"
+
+
+#define LED_DDR DDRB
+#define LED_PORT PORTB
+#define LED_PIN PINB
+#define LED_BIT _BV(m_nNumBit)
+#define NOP __asm__("nop\n\t")
+
+void Ai_WS2811::init(uint8_t pin, uint16_t nPixels) 
+{
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  m_nLeds = nPixels * 3; 
+  m_nCounter = 0; 
+  m_pData = (unsigned char*)malloc(m_nLeds); 
+  memset(m_pData,0,m_nLeds);
+  m_pDataEnd = m_pData + m_nLeds;
+  if( pin == 53 )
+  {
+    m_nNumBit = 0;
+  }
+  else if( pin == 52 )
+  {
+    m_nNumBit = 1;
+  }  
+  else if( pin == 51 )
+  {
+    m_nNumBit = 2;
+  }    
+  else if( pin == 50 )
+  {
+    m_nNumBit = 3;
+  } 
+  m_nDimCoef = 1;
+   
+}
+
+void Ai_WS2811::setDim( const int nDimCoef )
+{
+  m_nDimCoef = nDimCoef;
+}
+
+void Ai_WS2811::applyDim( void )
+{
+  if( m_nDimCoef == 1 )
+  {
+    return;
+  }
+  register byte *p = m_pData;
+  register byte *e = m_pDataEnd;
+  while(p != e) 
+  { 
+     (*p) /= m_nDimCoef; ++p;
+   }
+}
+
+void Ai_WS2811::setColor(unsigned char r,unsigned char g,unsigned char b)
+{
+  Serial.print("setColor " );
+  Serial.print(r);
+  Serial.print( ", " );
+  Serial.print(g);
+  Serial.print( ", " );
+  Serial.print(b);
+  Serial.println( "" );
+
+  register byte *p = m_pData;
+  register byte *e = m_pDataEnd;
+  while(p != e) 
+  { 
+     *p++ = g;
+     *p++ = r;
+     *p++ = b;
+  }  
+  sendLedData();
+}
+
+void Ai_WS2811::setVumeter( int nValue,int bR, int bG, int bB )
+{
+  int nNbrPixel = m_nLeds/3;
+  int nNbrValuePerPixel = 10000/nNbrPixel;
+  int nNbrPixelToLighten = nValue / nNbrValuePerPixel;
+  int nNbrRemaining = nValue - (nNbrPixelToLighten * nNbrValuePerPixel);
+  
+  nNbrRemaining = map( nNbrRemaining, 0, nNbrValuePerPixel, 0, 255 );
+  struct CRGB * leds = (struct CRGB *)m_pData;
+  int i;
+  for( i = 0; i < nNbrPixelToLighten; ++i )
+  {
+    if( bR ) leds[i].r = 255; else leds[i].r = 0;
+    if( bG ) leds[i].g = 255; else leds[i].g = 0;
+    if( bB ) leds[i].b = 255; else leds[i].b = 0;
+  }
+  if( i < nNbrPixel )
+  {
+    if( bR ) leds[i].r = nNbrRemaining; else leds[i].r = 0;
+    if( bG ) leds[i].g = nNbrRemaining; else leds[i].g = 0;
+    if( bB ) leds[i].b = nNbrRemaining; else leds[i].b = 0;
+    ++i;
+  }
+  for(; i < nNbrPixel; ++i )
+  {
+    leds[i].r = 0;
+    leds[i].g = 0;
+    leds[i].b = 0;    
+  }
+  sendLedData();
+}
+
+void Ai_WS2811::setOnlyOne( unsigned int nIdx, uint8_t r, uint8_t g, uint8_t b )
+{
+  int nNbrPixel = m_nLeds/3;  
+  struct CRGB * leds = (struct CRGB *)m_pData;
+  memset( m_pData,0,m_nLeds );
+  long i = (long)nIdx%10000;
+  i = (long)(i*nNbrPixel)/10000;
+//  Serial.print(i);
+  leds[i].r = r;
+  leds[i].g = g;
+  leds[i].b = b;
+  sendLedData();
+}
+
+int Ai_WS2811::reducePixelNumber( int nNewPixelNumber )
+{
+  int nNewLedsNumber = nNewPixelNumber*3;
+  if( m_nLeds <= nNewLedsNumber )
+    return 0;
+    
+  m_nLeds = nNewLedsNumber;
+  m_pDataEnd = m_pData + m_nLeds;
+  return 1;
+}
+
+
+
+void Ai_WS2811::sendLedData(void)
+{
+  applyDim();
+  cli();
+  register byte *p = m_pData;
+  register byte *e = m_pDataEnd;
+  volatile uint8_t b;
+  while(p != e) 
+  { 
+    b   = *p++;    // Current byte value
+    byte i=8;
+    do {
+      if ((b&0x80)==0) {
+        // Send a '0'
+        if (F_CPU==16000000L) {
+          LED_PIN=LED_BIT;NOP;// Hi (start)
+          NOP;NOP;            // Hi
+          LED_PIN=LED_BIT;NOP;// Lo (250ns)
+          NOP;NOP;            // Lo
+          NOP;NOP;            // Lo (500ns)
+        }   
+        else if (F_CPU==8000000L) {
+          LED_PIN = LED_BIT;  // Hi (start)
+          NOP;                // Hi
+          LED_PIN = LED_BIT;  // Lo (250ns)
+          NOP;                // Lo
+          NOP;                // Lo (500ns)
+          NOP;                // Lo (data bit here!)  
+          NOP;                // Lo (750ns)
+        }   
+      }   
+      else {
+        // Send a '1'
+        if (F_CPU==16000000L) {
+          LED_PIN=LED_BIT;NOP;// Hi (start)
+          NOP;NOP;            // Hi
+          NOP;NOP;            // Hi (250ns)
+          NOP;NOP;            // Hi
+          NOP;NOP;            // Hi (500ns)
+          LED_PIN=LED_BIT;    // Lo (625ns)
+        }   
+        else if (F_CPU==8000000L) {
+          LED_PIN = LED_BIT;  // Hi (start)
+          NOP;                // Hi
+          NOP;                // Hi (250ns)
+          NOP;                // Hi
+          NOP;                // Hi (500ns)
+          NOP;                // Hi (data bit here!)
+          LED_PIN = LED_BIT;  // Lo (750ns)
+        }   
+      }   
+      b = b+b;
+    } while (--i!=0);
+  }
+  sei();
+}
--- arduino_prj/glasses_receiver/glasses_receiver.ino
+++ arduino_prj/glasses_receiver/glasses_receiver.ino
@@ -1,63 +1,143 @@
-#define LED_PIN 13
+#include "Ai_WS2811.h"
+
+
+#define LED_DEBUG_PIN 13
+#define LED_RGB_PIN 53
+
+#define NUM_PIXELS 8
+Ai_WS2811 ws2811;
+struct CRGB * pLeds = NULL;
+
 
 void debug_with_led( unsigned char B )
 {
   // Print a long then one impulsion per byte
-  digitalWrite(LED_PIN, HIGH);   
+  digitalWrite(LED_DEBUG_PIN, HIGH);   
   delay( 1000 );
-  digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_DEBUG_PIN, LOW);
   delay( 100 );          
   for( int i = 0; i < 8; ++i )
   {
     if( (1<<i) & B )
     {
-      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(LED_DEBUG_PIN, HIGH);
     }
     else
     {
-      digitalWrite(LED_PIN, LOW);
+      digitalWrite(LED_DEBUG_PIN, LOW);
     }
     delay( 400 );
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_DEBUG_PIN, LOW);
     delay( 100 );      
   }
 }
 
-void setup()
+
+void setLeds( unsigned char * buf )
 {
+  // change color to buf, with buf: "BGR" composantes in [0..127]
+  Serial.print( "INF: setLeds: " );
+  Serial.print( buf[0] );
+  Serial.print( ", " );
+  Serial.print( buf[1] );
+  Serial.print( ", " );
+  Serial.print( buf[2] );  
+  Serial.println( "." );
+  ws2811.setColor( buf[2]*2, buf[1]*1, buf[0]*2 );
+}
+
+void checkLedsRGB( void )
+{
+  Serial.println( "INF: checkLedsRGB: begin..." );
+  unsigned char full[] = {127, 127, 127};
+  unsigned char r[] = {0, 0, 127};
+  unsigned char g[] = {0, 127,  0 };
+  unsigned char b[] = {127, 0, 0};  
+  const int nTimeDelay = 300;
+  setLeds( r );
+  delay( nTimeDelay );
+  setLeds( g );  
+  delay( nTimeDelay );  
+  setLeds( b );
+  delay( nTimeDelay );  
+  setLeds( full );
+  delay( nTimeDelay );  
+  Serial.println( "INF: checkLedsRGB: end" );  
+}
+
+void setup()
+{  
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  for( int i = 0; i < 10; ++i )
+  Serial.println( "INF: CdL: Glasses_Receiver v0.6" );
+  
+  pinMode(LED_DEBUG_PIN, OUTPUT);
+  for( int i = 0; i < 5; ++i )
   {
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(LED_DEBUG_PIN, HIGH);
     delay( 100 );
-    digitalWrite(LED_PIN, LOW);    
+    digitalWrite(LED_DEBUG_PIN, LOW);    
     delay( 200 );      
   }
-  
+
   // debug_with_led( 0 );
-  // debug_with_led( 0xFF );  
+  // debug_with_led( 0xFF );
+  
+  pinMode( LED_RGB_PIN, OUTPUT );
+  ws2811.init(LED_RGB_PIN,NUM_PIXELS);
+  pLeds = (struct CRGB*)ws2811.getRGBData();
+  ws2811.setDim( 32 );  
+  
+  checkLedsRGB();  
 }
 
 //unsigned char nLedValue = 0;
+const int nSizeBufMax = 3*4; // 3 should be enough
+unsigned char buf[nSizeBufMax] = ""; 
+int nSizeBuf = 0;
 void loop()
 {
     while(Serial.available())
     {
-      unsigned char buf = Serial.read();
-      if( 0 ) debug_with_led( buf );
-      
-      if( 1 )
+      unsigned char c = Serial.read();
+      if( 0 ) debug_with_led( c );
+      if( 0 )
       {
-        if( buf >= 50 )
+        if( c >= 50 )
         {
-          digitalWrite(LED_PIN, HIGH);
+          digitalWrite(LED_DEBUG_PIN, HIGH);
           // delay(1000);
         }
         else
         {
-          digitalWrite(LED_PIN, LOW);
+          digitalWrite(LED_DEBUG_PIN, LOW);
         }
       }
+      
+      if( c == 0 )
+      {
+        // end of message        
+        if( nSizeBuf == 3 )
+        {
+          setLeds( buf );
+        }
+        else
+        {
+          Serial.print( "ERR: at end of buffer, buffer hasn't the right side, nSizeBuf: " );
+          Serial.println( nSizeBuf );
+        }
+        nSizeBuf = 0;
+      }
+      else
+      {
+        // other chars
+        buf[nSizeBuf] = c; ++nSizeBuf;
+        if( nSizeBuf >= nSizeBufMax )
+        {
+          Serial.print( "ERR: when adding a new byte, nSizeBuf more than max: " );
+          Serial.println( nSizeBuf );
+          nSizeBuf = 0;
+        }
+      }
+      
     }
 }

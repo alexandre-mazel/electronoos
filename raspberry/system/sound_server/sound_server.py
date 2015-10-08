2b2da7e579d88acb587559c9846d1b5089480a9f
alexandre-mazel <alexandre.zelma@gmail.com>
2015-10-08 18:31:17 +0200

    final version
    Signed-off-by: alexandre-mazel <alexandre.zelma@gmail.com>

--- raspberry/system/sound_server/sound_server.py
+++ raspberry/system/sound_server/sound_server.py
@@ -1,50 +1,72 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+import SimpleHTTPServer
+import SocketServer
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
 
-class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+def analysePath( strPath ):
+    """
+    analyse a html request path 
+    - strPath: a path and command, eg: "/playsound?file=../toto.wav"
+    return an array: [filename, directory_name, parameters]
+    with:
+    - filename: the name of the file, eg: "playsound"
+    - directory_name: the requested location, eg: "/"
+    - parameters: a list of parameters pair, eg: [["file", "../toto.wav]]
+    """
     
-    def __init__( self ):
-        print( "INF: MyHTTPRequestHandler: init" )
+    astrPathSplittedMark = strPath.split( "?")
+    strPathWithoutParameters = astrPathSplittedMark[0]
+    strDirectoryName = os.path.dirname( os.path.abspath(strPathWithoutParameters) )
+    #~ print( "strDirectoryName: %s" % strDirectoryName )
     
-    #handle GET command
+    strFilename = os.path.basename( strPathWithoutParameters )
+    print( "strFilename: %s" % strFilename )
+    
+    if( len( astrPathSplittedMark ) > 1 ):
+        parameters = []
+        aRawParameters = astrPathSplittedMark[1].split("=")
+        for i in range(0,len(aRawParameters),2):
+            parameters.append([ aRawParameters[i], aRawParameters[i+1] ] )
+
+    return [strFilename, strDirectoryName, parameters]
+    
+# analysePath - end
+#~ retval = analysePath( "/playsound?file=../toto.wav" )    
+#~ print( "analysePath: %s" % retval );
+#~ exit()
+
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-        print( "INF: MyHTTPRequestHandler.do_GET: path: %s" % str(self.path) )
+        print( "INF: MyRequestHandler.get: path: %s" % self.path )
+        
+        # parse command
+        retval = analysePath(self.path)
+        print( "analysePath return: %s" % retval )
+        strPageName, dirName, params = retval
+        
+        # playsound?file=toto.wav
+        if( strPageName == "playsound" ):
+            print( "INF: MyRequestHandler.get: playsound: params: %s" % params )
+            for param in params:
+                name, value = param
+                if( name == "file" ):
+                    print( "playing file: %s" % value )
+                    self.wfile.write("Playing file: '%s'...<br>\n" % value)
+                    self.wfile.flush()
+                    retcall = os.system( "aplay %s" % value )
+                    print( "retcall : %s" % retcall )
+                    self.wfile.write("Return: Success: %s" % (retcall==0))
+            return;
 
-        rootdir = './' #file location
-        try:
-            if self.path.endswith('.html'):
-                f = open(rootdir + self.path) #open requested file
+        
+        #~ if self.path == '/':
+            #~ self.path = '/simplehttpwebpage_content.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-                #send code 200 response
-                self.send_response(200)
+Handler = MyRequestHandler
+#~ print(dir(Handler))
+server = SocketServer.TCPServer(('0.0.0.0', 8000), Handler)
 
-                #send header first
-                self.send_header('Content-type','text-html')
-                self.end_headers()
-
-                #send file content to client
-                self.wfile.write(f.read())
-                f.close()
-                return
-
-        except IOError:
-            self.send_error(404, 'file not found')
-          
-# class MyHTTPRequestHandler - end
-          
-def run():
-  print('http server is starting...')
- 
-  #ip and port of server
-  #by default http server port is 80
-  server_address = ('127.0.0.1', 8000)
-  httpd = HTTPServer(server_address, MyHTTPRequestHandler)
-  #~ httpd = HTTPServer(server_address, BaseHTTPRequestHandler)
-  print('http server is running...')
-  httpd.serve_forever()
-  
-if __name__ == '__main__':
-  run()
+print( "running..." )
+server.serve_forever()

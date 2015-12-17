107e3f881a76288ecd61eb7b71acd87a9b4f6af1
alexandre-mazel <alexandre.zelma@gmail.com>
2015-12-17 14:36:19 +0100

    def 2 hands

--- arduino_prj/electrosuit/electrosuit.ino
+++ arduino_prj/electrosuit/electrosuit.ino
@@ -15,44 +15,41 @@ const char pad[3] PROGMEM = { 0, 0, 0 };
 #include "Ai_WS2811.h"
 //#include <../../../../libraries/EEPROM/EEPROM.h> // => generate error when in a .c
 
-const int nNbrReader = 3;
+const int nNbrLedBands = 3;
 const int nLenCode = 12;
 
 const int nFirstLedPin = 50; // one led per reader
-const int nFirstPresencePin = 40; // one pin per reader
 
-const int nLedPinSendGood3 = 22;
-const int nLedPinReceiveGood3 = 24;
-
+const int nButtonPin = 40;
 
 #define NUM_PIXELS 48
-Ai_WS2811 aWs2811[nNbrReader];
-struct CRGB * apLeds[nNbrReader] = {NULL, NULL, NULL};
+Ai_WS2811 aWs2811[nNbrLedBands];
+struct CRGB * apLeds[nNbrLedBands] = {NULL, NULL, NULL};
 
 void check_leds( void )
 {
   // check all leds are ok: make the led blink all once (for startup)
   Serial.println( "check_leds..." );
   int i;
-  for( i = 0; i < nNbrReader; ++i )
+  for( i = 0; i < nNbrLedBands; ++i )
   {
     aWs2811[i].setColor( 255, 0, 0 );
   }
   delay( 1000 );
   
-  for( i = 0; i < nNbrReader; ++i )
+  for( i = 0; i < nNbrLedBands; ++i )
   {
     aWs2811[i].setColor( 0, 255, 0 );    
   }
   delay( 1000 );  
   
-  for( i = 0; i < nNbrReader; ++i )
+  for( i = 0; i < nNbrLedBands; ++i )
   {
     aWs2811[i].setColor( 0, 0, 255 );
   }
   delay( 1000 );  
 
-  for( i = 0; i < nNbrReader; ++i )
+  for( i = 0; i < nNbrLedBands; ++i )
   {
     aWs2811[i].setColor( 0, 0, 0 );
   }
@@ -68,23 +65,22 @@ void setup()
 {
   pad[0];
   int i;
+  
   Serial.begin(9600);
-  // setup pin
-  for( i = 0; i < nNbrReader; ++i )
-  {
-    pinMode( nFirstLedPin+i, OUTPUT );
-  }
 
-  for( i = 0; i < nNbrReader; ++i )
+
+  for( i = 0; i < nNbrLedBands; ++i )
   {
+    pinMode( nFirstLedPin+i, OUTPUT );    
     aWs2811[i].init(nFirstLedPin+i,NUM_PIXELS);
     apLeds[i] = (struct CRGB*)aWs2811[i].getRGBData();
     aWs2811[i].setDim( 1 );
   }
 //  aWs2811[2].reducePixelNumber( 4 );  
   
-  
   //check_leds();
+  
+  pinMode(nButtonPin, INPUT);
 
   
   Serial.println( "\nArduino started: ElectroSuit v0.6\n" );
@@ -97,7 +93,8 @@ int nNumFrame = 0;
 int nNumSequence = 0;
 int nBackGroundSequence = 0;
 int nBackGroundSequenceInc = 1;
-void loop()
+
+void handHeartBeat()
 {
   if( nNumFrame >= 12 ) // the 11 will light off
   {
@@ -120,7 +117,7 @@ void loop()
     //delay( 400 );
   }
   
-  Serial.println( nBackGroundSequence, DEC );
+  //Serial.println( nBackGroundSequence, DEC );
   
   
   uint8_t r, g, b;
@@ -131,20 +128,32 @@ void loop()
   //r = g = b = 255;
   //hueToRGB( (nNumSequence + 255) % 255, &r, &g, &b );
   r = 100; g = 255; b = 100;
-  if( 0 )
+  
+  for( int nNumHand = 0; nNumHand < 2; ++nNumHand )
+  {
+    aWs2811[nNumHand].setOneBrightOtherLow( 8, 0, nNumFrame-1, r, g, b, rLow, gLow, bLow );
+    aWs2811[nNumHand].setOneBrightOtherLow( 10, 8, 10-nNumFrame, r, g, b, rLow, gLow, bLow );
+    aWs2811[nNumHand].setOneBrightOtherLow( 11, 18, nNumFrame, r, g, b, rLow, gLow, bLow );
+    aWs2811[nNumHand].setOneBrightOtherLow( 10, 29, (10-nNumFrame)-1, r, g, b, rLow, gLow, bLow );
+    aWs2811[nNumHand].setOneBrightOtherLow( 4, 39, nNumFrame, r, g, b, rLow, gLow, bLow );  
+    aWs2811[nNumHand].sendLedData();
+  }
+  ++nNumFrame;
+  delay( 30 );      
+}
+void loop()
+{   
+  int nRead = digitalRead( nButtonPin );  
+//  Serial.println( nRead, DEC );
+  
+  if( nRead == HIGH )
   {
     // flash!
     aWs2811[0].setColor( 255,255,255 );
+    delay( 3 );
   }
   else
   {
-    aWs2811[0].setOneBrightOtherLow( 8, 0, nNumFrame-1, r, g, b, rLow, gLow, bLow );
-    aWs2811[0].setOneBrightOtherLow( 10, 8, 10-nNumFrame, r, g, b, rLow, gLow, bLow );
-    aWs2811[0].setOneBrightOtherLow( 11, 18, nNumFrame, r, g, b, rLow, gLow, bLow );
-    aWs2811[0].setOneBrightOtherLow( 10, 29, (10-nNumFrame)-1, r, g, b, rLow, gLow, bLow );
-    aWs2811[0].setOneBrightOtherLow( 4, 39, nNumFrame, r, g, b, rLow, gLow, bLow );  
-    aWs2811[0].sendLedData();
+    handHeartBeat();
   }
-  ++nNumFrame;
-  delay( 30 );  
 }

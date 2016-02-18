4361659d8a186fb927675659ad41b1ff00288049
Alexandre Mazel <amazel@aldebaran-robotics.com>
2016-02-18 22:48:28 +0100

    add auto draw classifying rect and circle and redrawing

--- fastscheme/fastscheme.py
+++ fastscheme/fastscheme.py
@@ -7,6 +7,7 @@
 
 import cv2
 
+import math
 import mutex
 import numpy
 import sys
@@ -20,6 +21,17 @@ def computePerimeter(shape):
     for i in range(len(shape)-1): # we don't add the distance between last and first
         rDist += geo.distance( shape[i], shape[(i+1)%len(shape)] ) # modulo in the case of we want the distance to first and last
     return rDist
+    
+def generateCircle( center, radius ):
+    """
+    return a list of point to draw a circle
+    """
+    nNbrPoint = 64
+    shape = []
+    for i in range( nNbrPoint+1 ):
+        rRad = i*2.*math.pi/nNbrPoint
+        shape.append( [ int(center[0] + radius*math.cos(rRad)), int(center[1] + radius*math.sin(rRad))  ] )
+    return shape
 
 class FastScheme:
     def __init__( self, nResolutionW, nResolutionH ):
@@ -88,16 +100,42 @@ class FastScheme:
     def analyseShape( self, shape ):
         """
         analyse a new shape
+        return 1 if closed shape
         """
+        nRet = 0
         rPerimeter = computePerimeter( shape )
         print( "rPerimeter: %s" % rPerimeter )        
         rLastFirstDist = geo.distance( shape[0], shape[-1] )
         print( "rLastFirstDist: %s" % rLastFirstDist )
+        bb = geo.computeBoudingBox( shape )
+        print( "bb: %s" % bb )
+        rSizeBB = geo.distance( bb[0], bb[1] )
+        print( "rSizeBB: %s" % rSizeBB )
+        cornerBB = [ [bb[0][0],bb[0][1]], [bb[1][0],bb[0][1]], [bb[1][0],bb[1][1]], [bb[0][0],bb[1][1]]  ]
+        rDistToBB = geo.compute_distance_to_points( shape, cornerBB )
+        print( "rDistToBB: %s" % rDistToBB )
         if( rLastFirstDist * 6 < rPerimeter ): # 8
-            # nearly bClosedFigure
+            # nearly ClosedFigure            
             shape.append( shape[0] )
-            self.listClosedFiguresRect.append(shape[:])
+            if( rDistToBB < rSizeBB *0.4 ):
+                print( "Rectangle!")
+                shape = cornerBB
+                shape.append(cornerBB[0])
+                self.gridifyShape( shape )
+                self.listClosedFiguresRect.append(shape[:])
+            else:
+                print( "Circle!")
+                center = geo.median( bb[0], bb[1] )
+                self.gridify( center )
+                radius = abs( bb[0][0]-center[0] )
+                radius = ((radius+(self.nGridSize/2))/self.nGridSize)*self.nGridSize
+                shape = generateCircle( center, radius )
+                #self.gridifyShape( shape )
+                self.listClosedFiguresCirc.append(shape[:])
+            nRet = 1
         print( "INF: FastScheme.analyseShape: at end: %s" % self.__str__() )
+        return nRet
+    # analyseShape - end
             
             
 
@@ -114,7 +152,8 @@ class FastScheme:
             time.sleep(0.01)        
         self._mouseMove( x, y )
         self.bMouseDown = False
-        self.analyseShape( self.listPts[-1] )
+        if( self.analyseShape( self.listPts[-1] ) ):
+            self.listPts = []
         self.mutexMouse.unlock()
         
     def _mouseMove( self, x, y ):
@@ -134,7 +173,21 @@ class FastScheme:
         pt[0] = ((pt[0]+(self.nGridSize/2))/self.nGridSize)*self.nGridSize
         pt[1] = ((pt[1]+(self.nGridSize/2))/self.nGridSize)*self.nGridSize                
         
+    def gridifyShape( self, shape ):
+        for pt in shape:
+            self.gridify( pt )
         
+    def addFakeMouseMovements( self, listlistPts ):
+        """
+        Simulate a mouse draing a list of shapes.
+        - listlistPts: a list of list of points (one list per shape)
+        """
+        for listPts in listlistPts:
+            self.mouseDown( listPts[0][0], listPts[0][1] )
+            for pt in listPts[1:-1]:
+                self.mouseMove( pt[0], pt[1] )    
+            self.mouseUp( listPts[-1][0], listPts[-1][1] )
+            
     def update( self ):
         pass
         
@@ -155,12 +208,12 @@ class FastScheme:
             else:
                 bLastOne = 0
             pt1 = shape[0]
-            if( not bLastOne ):
-                self.gridify(pt1)
+            #~ if( not bLastOne ):
+                #~ self.gridify(pt1)
             
             for pt2 in shape[1:]:
-                if( not bLastOne ):
-                    self.gridify(pt2)
+#                if( not bLastOne ):
+#                    self.gridify(pt2)
                 cv2.line( img, (pt1[0], pt1[1]), (pt2[0], pt2[1]), (255,0,0) )
                 pt1 = pt2
         # for listPts - end
@@ -178,7 +231,10 @@ class FastScheme:
         
 # class FastScheme - end
 
-def runApp():
+def runApp(initFakeListListPts = None):
+    """
+    initFakeListListPts: a list of list of points, see addFakeMouseMovements
+    """
     nCptFrameFps = 0
     timeBeginFps = time.time()
     nNbrFrameToComputeFps = 100
@@ -192,6 +248,8 @@ def runApp():
         nSizeY /=2
         
     fast = FastScheme(nSizeX, nSizeY)
+    if( initFakeListListPts != None ):
+        fast.addFakeMouseMovements(initFakeListListPts)
 
 
     def on_mouse_event(event, x, y, flags, param):
@@ -234,5 +292,6 @@ def runApp():
     print( "INF: runApp: finished..." )
 # runApp - end
 
-runApp()
+listlistPts = [[[468, 290], [462, 290], [459, 290], [456, 290], [453, 290], [451, 291], [448, 292], [445, 294], [443, 295], [441, 298], [438, 303], [432, 314], [428, 323], [425, 330], [423, 336], [422, 340], [420, 346], [420, 353], [420, 363], [420, 372], [420, 383], [423, 392], [426, 401], [432, 408], [438, 416], [445, 422], [453, 428], [462, 435], [472, 441], [483, 445], [494, 449], [506, 449], [518, 449], [526, 449], [535, 448], [542, 446], [550, 441], [557, 436], [566, 430], [574, 424], [579, 415], [581, 407], [581, 398], [581, 386], [578, 377], [573, 369], [567, 360], [560, 352], [555, 347], [550, 341], [547, 335], [543, 329], [538, 326], [533, 322], [528, 318], [522, 315], [517, 311], [512, 308], [508, 302], [503, 299], [500, 297], [497, 296], [495, 296], [493, 295], [492, 295], [491, 295], [490, 295], [489, 294], [489, 294], [487, 294], [487, 294], [487, 295], [488, 295], [488, 295], [468, 290]], [[760, 224], [760, 226], [761, 229], [761, 235], [761, 243], [761, 254], [762, 269], [766, 283], [768, 300], [770, 316], [770, 330], [772, 336], [772, 340], [772, 341], [772, 341], [772, 341], [773, 341], [773, 341], [774, 341], [785, 341], [789, 341], [795, 341], [800, 341], [804, 341], [808, 341], [813, 340], [818, 339], [824, 337], [834, 334], [845, 334], [855, 332], [865, 332], [875, 332], [881, 332], [888, 333], [893, 333], [897, 335], [901, 335], [904, 336], [907, 336], [909, 336], [912, 335], [912, 334], [912, 334], [916, 331], [917, 330], [917, 330], [918, 324], [918, 321], [918, 315], [918, 311], [918, 306], [919, 301], [919, 294], [921, 288], [922, 282], [922, 276], [922, 273], [922, 271], [922, 268], [922, 265], [922, 260], [922, 258], [922, 257], [922, 257], [922, 256], [922, 254], [922, 253], [922, 252], [922, 251], [922, 249], [921, 248], [921, 247], [920, 245], [920, 244], [920, 243], [920, 240], [920, 237], [920, 234], [920, 232], [920, 231], [919, 231], [919, 231], [919, 231], [919, 231], [919, 231], [913, 229], [911, 228], [908, 228], [905, 228], [901, 229], [897, 229], [891, 229], [888, 229], [883, 228], [878, 228], [872, 227], [868, 227], [864, 227], [861, 227], [856, 227], [851, 227], [847, 227], [842, 227], [839, 227], [835, 227], [832, 227], [829, 227], [826, 226], [822, 226], [818, 226], [814, 226], [811, 226], [810, 226], [808, 225], [806, 225], [803, 225], [800, 225], [797, 225], [795, 225], [792, 225], [789, 225], [788, 224], [787, 224], [786, 222], [785, 222], [784, 222], [783, 222], [782, 222], [781, 222], [781, 222], [780, 222], [775, 222], [774, 222], [773, 222], [773, 222], [773, 222], [773, 223], [773, 223], [760, 224]], [[801, 466], [801, 467], [801, 468], [801, 470], [802, 473], [802, 476], [802, 481], [803, 489], [804, 497], [805, 505], [807, 512], [808, 519], [809, 526], [810, 532], [810, 537], [811, 544], [811, 551], [812, 559], [812, 565], [813, 568], [813, 571], [814, 572], [814, 573], [815, 573], [815, 574], [816, 575], [817, 576], [819, 577], [820, 577], [823, 578], [828, 579], [834, 580], [841, 581], [852, 581], [864, 583], [878, 585], [892, 586], [906, 587], [918, 589], [929, 589], [936, 589], [940, 589], [943, 589], [945, 589], [946, 589], [947, 589], [947, 589], [947, 589], [951, 585], [952, 582], [953, 579], [953, 573], [956, 565], [958, 554], [961, 540], [962, 524], [964, 507], [964, 491], [964, 481], [964, 473], [964, 469], [964, 467], [963, 466], [963, 465], [962, 464], [962, 464], [962, 463], [962, 463], [962, 463], [960, 463], [959, 462], [956, 461], [952, 461], [948, 460], [942, 460], [936, 459], [928, 459], [920, 459], [910, 459], [899, 459], [891, 461], [881, 462], [872, 462], [863, 463], [856, 463], [850, 464], [844, 464], [839, 465], [836, 465], [834, 465], [833, 465], [831, 465], [830, 465], [829, 465], [826, 465], [823, 465], [820, 465], [818, 465], [815, 465], [813, 465], [811, 465], [810, 466], [808, 466], [808, 466], [807, 466], [807, 465], [807, 465], [801, 466]]]
+runApp(listlistPts)
 print( "finished..." )

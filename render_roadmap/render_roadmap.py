eebf1c621176795930186b47c6d283b5da81988c
alexandre-mazel <alexandre.zelma@gmail.com>
2016-03-14 18:59:59 +0100

    up

--- render_roadmap/render_roadmap.py
+++ render_roadmap/render_roadmap.py
@@ -15,3 +15,19 @@ import time
 
 sys.path.insert( 0, "../../protolab" )
 import protolab.geometry as geo
+
+def renderRoadMap(aListTask):
+    nSizeX = 800
+    nSizeY = 600
+    img = numpy.zeros((nSizeY,nSizeX,3), numpy.uint8)
+    img[::] = (255,255,255)
+    windowName = "roadmap"
+    cv2.namedWindow( windowName )
+    cv2.imshow( windowName, img )
+    cv2.waitKey(1500)
+    
+# renderRoadMap - end    
+
+
+taskList = []
+renderRoadMap( taskList )

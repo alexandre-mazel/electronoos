bf745c7037515bdc10850a0a068a4c0de22f3bad
alexandre-mazel <alexandre.zelma@gmail.com>
2017-02-03 01:30:08 +0100

    running version

--- capture/capture.py
+++ capture/capture.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
+import os
 import time
+
+#~ from skimage.measure import structural_similarity as ssim # apt-get install python-skimage
+#~ import paramiko
 
 # Captures a single image from the camera and returns it in PIL format
 def get_image(camera):
@@ -30,6 +35,25 @@ def mse(imageA, imageB):
 	# the two images are
 	return err
 
+def sendFileToRemoteParamiko( server, user, srcfilename, dstfilename ):
+    import paramiko
+    def createSSHClient(server, port, user, password):
+        
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(server, port, user, password)
+        return client
+    ssh = createSSHClient("protolab.aldebaran.com", 22, "", "")
+    scp = SCPClient(ssh.get_transport())
+    #~ Then call scp.get() or scp.put() to do scp operations.
+    # TODO: finish me !
+    
+def sendFileToRemote( server, user, srcfilename, dstfilename ):
+    strCommand = "scp %s %s@%s:%s" % (srcfilename, user, server,  dstfilename)
+    print( "INF: running command: '%s'" % strCommand )
+    os.system( strCommand )
+
 def watchAndSend( nNumCameraPort ):
     #Number of frames to throw away while the camera adjusts to light levels
     ramp_frames = 1 # 30
@@ -39,6 +63,8 @@ def watchAndSend( nNumCameraPort ):
     # Now we can initialize the camera capture object with the cv2.VideoCapture class.
     # All it needs is the index to a camera port.
     camera = cv2.VideoCapture(camera_port)
+    camera.set(3,640)
+    camera.set(4,480)    
 
     print("INF: Warming up...")         
     # Ramp the camera - these frames will be discarded and are only used to allow v4l2
@@ -51,20 +77,27 @@ def watchAndSend( nNumCameraPort ):
     
     imPrev = get_image(camera)
     imPrevGrey = cv2.cvtColor( imPrev, cv2.COLOR_BGR2GRAY )
-    while( 1 ):
-        print( "DBG: %s: getting..." % time.time() )
-        im = get_image(camera)
     
-        print( "DBG: %s: analysing..." % time.time() )
+
+    while( 1 ):
+        #~ print( "DBG: %s: getting..." % ti    me.time() )
+        im = get_image(camera) # it seems like we've got a buffer of 6 images, so even if taking time to send, we've got 6 images of moving datas in raw stored! great
+    
+        #~ print( "DBG: %s: analysing..." % time.time() )
         imGrey = cv2.cvtColor( im, cv2.COLOR_BGR2GRAY )
         #rDiff = computeDiff( imGrey, imPrevGrey )
         rDiff = mse( imGrey, imPrevGrey )
-        print( "DBG: rDiff: %s" % str(rDiff) )
+        #~ rDiff = ssim(mGrey, imPrevGrey )
+        print( "DBG: %s: rDiff: %s" % ( time.time(), str(rDiff)) )
         
-        if rDiff > 100:
+        if rDiff > 120:
             print( "DBG: %s: storing..." % time.time() )
-            file = "/home/pi/images/%s/%s.jpg" % time.time() )
-            cv2.imwrite(file, im)
+            file = "/home/pi/images/%09.03f.jpg" % time.time()
+            #~ file = "/tmp/last.jpg" # to store nothing locally enable this line
+            cv2.imwrite(file, im,[int(cv2.IMWRITE_JPEG_QUALITY), 50])
+            dstFile = "~/images/%09.03f.jpg"  % time.time()
+            sendFileToRemote( "protolab.aldebaran.com", "amazel", file, dstFile )
+            #~ sendImage(
         
         imPrevGrey = imGrey
      

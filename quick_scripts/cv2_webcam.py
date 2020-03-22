093a15fbec4798601a2d46441021544ea0ca3db6
Alexander Mazel <amazel@aldebaran.com>
2020-03-22 23:19:51 +0100

    up et aussi pour manuel scolaire

--- quick_scripts/cv2_webcam.py
+++ quick_scripts/cv2_webcam.py
@@ -2,7 +2,63 @@ import numpy as np
 import cv2
 import time
 
-cap = cv2.VideoCapture(0)
+import threading
+
+class VideoCaptureAsync:
+    """
+    found that on the web, author said it's faster, I think it doesn't wait for the image to be completed.
+    At least it doesn't stuck the main thread...
+    """
+    def __init__(self, src=0, width=640, height=480):
+        self.src = src
+        self.cap = cv2.VideoCapture(self.src)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.grabbed, self.frame = self.cap.read()
+        self.started = False
+        self.read_lock = threading.Lock()
+
+    def set(self, var1, var2):
+        self.cap.set(var1, var2)
+
+    def start(self):
+        if self.started:
+            print('[!] Asynchroneous video capturing has already been started.')
+            return None
+        self.started = True
+        self.thread = threading.Thread(target=self.update, args=())
+        self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            grabbed, frame = self.cap.read()
+            with self.read_lock:
+                self.grabbed = grabbed
+                self.frame = frame
+
+    def read(self):
+        with self.read_lock:
+            frame = self.frame.copy()
+            grabbed = self.grabbed
+        return grabbed, frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exec_type, exc_value, traceback):
+        self.cap.release()
+        
+# VideoCaptureAsync - end
+
+cap = cv2.VideoCapture(0) #or 0 + cv2.CAP_DSHOW
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
 
 while(True):
     # Capture frame-by-frame
@@ -16,7 +72,8 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    #~ cv2.imshow('frame',frame)
+    cv2.imshow('gray',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- quick_scripts/generate_pdf_to_print.py
+++ quick_scripts/generate_pdf_to_print.py
@@ -16,11 +16,10 @@ def pdfMultiCell( pdf, x, y, txt, hInterlign, bCentered = False ):
         pdf.text(xp,y,line )
         y += hInterlign
 
-def genereatePdfFromImages_and_text( listImgs, strOutPdfFilename, strVersoText = "", nOuputType=0 ):
+def generatePdfFromImagesAndText( listImgs, strOutPdfFilename, strVersoText = None, nNbrImagePerPage = 4, aListArea = None ):
     """
     Generate a pdf files from a list of images
-    nRectoVersoHandling:
-    - 0: print all
+    - aListArea: for each area one image will be generated and copied as a new image (one area per page if nbr_image_per_page = 1)
     """
     pdf = FPDF('P', 'mm', 'A4') # Portrait, measures in mm, format is A4
     #~ pdf.add_page()
@@ -32,48 +31,59 @@ def genereatePdfFromImages_and_text( listImgs, strOutPdfFilename, strVersoText =
     nNumImage = 0
     nNbrImagePerPage = 4
     bDoubleForVerso = False # prepare for being printed with verso corresponding to same image
+    nIdxArea = 0
+    nNbrImageThisPage = 1000
     while 1:
-        pdf.add_page()
-        for i in range(nNbrImagePerPage):
-            if nNumImage+i >= len(listImgs):
-                break
-            pdf.image(listImgs[nNumImage+i],x=int(nImageW*(i%2)), y=int(nImageH*(i//2)), w=nImageW)
-        if bDoubleForVerso:
+        if nNbrImageThisPage >= nNbrImagePerPage:
             pdf.add_page()
-            for i in range(nNbrImagePerPage):
-                if nNumImage+i >= len(listImgs):
-                    break
-                pdf.image(listImgs[nNumImage+i],x=int(nImageW*((i+1)%2)), y=int(nImageH*(i//2)), w=nImageW)
+            nNbrImageThisPage = 0
+
+        strFilename = listImgs[nNumImage]
+        if aListArea != None:
+            im = cv2.imread(strFilename)
+            r = aListArea[nIdxArea]
+            im = im[r[0]:r[2],r[1]:r[3]]
+            strFilename = "/tmp/crop.png"
+            cv2.imwrite(im, strFilename)
+            nIdxArea += 1
+            if nIdxArea <= len(aListArea):
+                nIdxArea = 0
+                nNumImage += 1
+        else:
+            nNumImage += 1
             
-        nNumImage += nNbrImagePerPage
+        pdf.image(strFilename,x=int(nImageW*(nNbrImageThisPage%2)), y=int(nImageH*(nNbrImageThisPage//2)), w=nImageW)
+        
+        nNbrImageThisPage += 1
         
         if nNumImage >= len(listImgs):
             break
             
-    # add some text
-    txt = strVersoText
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    #pdf.write(8, txt) # text as normal text
-    #pdf.cell(0,8, txt, 1, 0, 'c') # not handling multiline
-    # multi_cell to be tried
-    nMarginTop = 25
-    nMarginLeft = 0 # 10
-    bCentered = True
-    hInterlign = 8
-    nCenter = 105
-    x = 0
-    if bCentered: x+=nCenter/2
-    pdfMultiCell( pdf, x+nMarginLeft, 0+nMarginTop, txt, hInterlign, bCentered=bCentered )
-    pdfMultiCell( pdf, x+nCenter+nMarginLeft, 0+nMarginTop, txt, hInterlign, bCentered=bCentered )
-    pdfMultiCell( pdf, x+nMarginLeft, 148+nMarginTop, txt, hInterlign, bCentered=bCentered )
-    pdfMultiCell( pdf, x+nCenter+nMarginLeft, 148+nMarginTop, txt, hInterlign, bCentered=bCentered )
+    if strVersoText != None:
+        # add some text
+        txt = strVersoText
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 16)
+        #pdf.write(8, txt) # text as normal text
+        #pdf.cell(0,8, txt, 1, 0, 'c') # not handling multiline
+        # multi_cell to be tried
+        nMarginTop = 25
+        nMarginLeft = 0 # 10
+        bCentered = True
+        hInterlign = 8
+        nCenter = 105
+        x = 0
+        if bCentered: x+=nCenter/2
+        pdfMultiCell( pdf, x+nMarginLeft, 0+nMarginTop, txt, hInterlign, bCentered=bCentered )
+        pdfMultiCell( pdf, x+nCenter+nMarginLeft, 0+nMarginTop, txt, hInterlign, bCentered=bCentered )
+        pdfMultiCell( pdf, x+nMarginLeft, 148+nMarginTop, txt, hInterlign, bCentered=bCentered )
+        pdfMultiCell( pdf, x+nCenter+nMarginLeft, 148+nMarginTop, txt, hInterlign, bCentered=bCentered )
     
     print( "INF: genereatePdfFromImages: outputting to '%s'" % (strOutPdfFilename) )
     pdf.output( strOutPdfFilename, 'F' )
+# generatePdfFromImagesAndText - end
     
-        
-if 1:
+def generateGaiaBirthday():
     fn = "C:/Users/amazel/perso/retouches/anniv_gaia_7_affiche copy.png"
     listImages = [fn]*4
     strVersoText = """
@@ -90,4 +100,20 @@ if 1:
     avant le 25 Mars,
     au 06.10.60.19.79
     """
-    genereatePdfFromImages_and_text(listImages, '/tmp/generated.pdf', strVersoText)
+    generatePdfFromImagesAndText(listImages, '/tmp/generated.pdf', strVersoText, nNbrImagePerPage = 4)
+# generateGaiaBirthday - end
+
+def generateSchoolBook():
+    strSkul = "C:/Users/amazel/perso/manuel_scolaire/cp_je_lis_tome1__%03d.png"
+    listImages = []
+    aListArea = [(320,334,1344,1480),(1392,334,2386,1480)] # one area per page
+    for nNumPage in range(500):
+        strFileName = strSkul%nNumPage
+        if os.path.isfile( strFileName ):
+            listImages.append(strFileName)
+            
+    generatePdfFromImagesAndText(listImages, '/tmp/generated.pdf', nNbrImagePerPage = 1, aListArea=aListArea)
+# generateSchoolBook - end
+
+#~ generateGaiaBirthday()
+generateSchoolBook()

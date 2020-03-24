4e9c3cb4d405935e3efb27be58060ec6d644e389
Alexandre Mazel <alexandre.zelma@gmail.com>
2020-03-24 19:04:57 +0000

    up

--- quick_scripts/stat_connected.py
+++ quick_scripts/stat_connected.py
@@ -3,23 +3,40 @@ import time
 
 def runCommandGetResults( strCommand ):
     strFilename = "/tmp/" + str(time.time())
-    os.system(strCommand + " > " + strCommand )
+    os.system(strCommand + " > " + strFilename )
     f = open(strFilename, "rt")
     buf = f.read()
     f.close()
     os.remove(strFilename)
     return buf
-
+    
 def getHostUp():
     strFirstIP = "192.168.0.1" # TODO: auto detect
-    buf = runCommandGetResults( "sudo nmap -sP %s/24 nmap" % strFirstIP )
+    buf = runCommandGetResults( "nmap -sP %s-10 -v" % strFirstIP )
+    lines = buf.split("\n")
+    bInConnected = False
+    strIP = ""
+    for l in lines:
+        print("l: %s" % l )
+        if "Nmap scan report" in l and not "[host down]" in l:
+            bInConnected = True
+            idx = l.find( "for " )
+            strIP = l[idx:]
+            print( "strIP: %s" % strIP )
+            
+    
     """
 Nmap scan report for 192.168.0.5
 Host is up (0.00092s latency).
-MAC Address: B8:27:EB:C1:69:F7 (Raspberry Pi Foundation)"""
+MAC Address: B8:27:EB:C1:69:F7 (Raspberry Pi Foundation)
+"""
 
     print(buf)
     
     
-def updateConnected()
+def updateConnected():
+    getHostUp()
+    
+    
+updateConnected()
     

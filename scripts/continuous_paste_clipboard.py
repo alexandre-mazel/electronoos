50225264a82ab79d1189f0d090e09bf6afae5c05
Alexander Mazel <amazel@aldebaran.com>
2020-05-08 21:59:28 +0200

    finito

--- scripts/continuous_paste_clipboard.py
+++ scripts/continuous_paste_clipboard.py
@@ -8,6 +8,23 @@ if strLocalPath == "": strLocalPath = './'
 sys.path.append(strLocalPath+"/../alex_pytools/")
 import misctools
 
+import time
+
+def isPilImgEqual(im1, im2):
+    if im1 == None or im2 == None:
+        return False
+        
+    from PIL import ImageChops
+    return ImageChops.difference(im1, im2).getbbox() is None
+    
+    #~ return im1.getdata() == im2.getdata() # compare just adress?
+# isPilImgEqual - end
+
+def bipInform():
+    import winsound
+    frequency = 500 # Set Frequency To 2500 Hertz
+    duration = 50  # Set Duration To 1000 ms == 1 second
+    winsound.Beep(frequency, duration)
 
 def continuousSave():
     print("INF: continuousSaveOfClipbBoardImage...")
@@ -15,11 +32,17 @@ def continuousSave():
     while 1:
         img = ImageGrab.grabclipboard()
         # or ImageGrab.grab() to grab the whole screen!
-        if img != img_prev:
+        if img == None:
+            time.sleep(0.2)
+            continue
+        if not isPilImgEqual( img, img_prev ):
             img_prev = img
             name = "/tmp/" + misctools.getFilenameFromTime() + ".png"
             print( "INF: saving to '%s'" % name ) 
             img.save( name, 'PNG' )
+            bipInform()
+        else:
+            time.sleep(0.2)
 # continuousSave - end
 
 

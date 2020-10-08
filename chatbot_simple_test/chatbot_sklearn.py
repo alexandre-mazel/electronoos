2c05231d22e6b83de18920af84efd65c53908c86
Alexander Mazel <amazel@aldebaran.com>
2020-10-08 21:37:54 +0200

    up

--- chatbot_simple_test/chatbot_sklearn.py
+++ chatbot_simple_test/chatbot_sklearn.py
@@ -1,9 +1,14 @@
 # simple chatbot exploration for education purpose
 # started from https://medium.com/analytics-vidhya/building-a-simple-chatbot-in-python-using-nltk-7c8c8215ac6e
 
+# later look at: https://chatbotslife.com/how-to-create-an-intelligent-chatbot-in-python-c655eb39d6b1
+# https://www.tophebergeur.com/blog/projet-chatbot-python/
+# https://towardsdatascience.com/how-to-create-a-chatbot-with-python-deep-learning-in-less-than-an-hour-56a063bdfc44
+
 import nltk
 import numpy as np
 import random
+import re
 import string # to process standard python strings
 
 if 0:
@@ -13,6 +18,14 @@ if 0:
 f=open('chatbot.txt','r',errors = 'ignore')
 raw=f.read()
 raw=raw.lower()# converts to lowercase
+
+# rough preprocessing.
+raw=raw.replace("...", ".")
+raw=raw.replace("..", ".")
+raw=raw.replace("[4]", "")
+#raw = re.sub("[\(\[].*?[\)\]]", "", raw) # replace [23] et (blabla)
+raw = re.sub("[\[].*?[\]]", "", raw) # replace [23] 
+
 
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
@@ -37,7 +50,9 @@ def greeting(sentence):
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+global_strPrev = None
 def response(user_response):
+    global global_strPrev
     robo_response=''
     sent_tokens.append(user_response)    
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
@@ -47,15 +62,21 @@ def response(user_response):
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]    
-    if(req_tfidf==0):
-        print( "user_response: %s" % user_response )
-        if "what" in user_response or "who" in user_response or "how" in user_response:
-            robo_response = robo_response+"I don't know, sorry!"
-        else:
-            robo_response = robo_response+"I am sorry! I don't understand you"
-        return robo_response
 
-    robo_response = robo_response+"What I Know: " + sent_tokens[idx]
+    if(req_tfidf==0):
+        #~ print( "user_response: %s" % user_response )
+        if user_response == global_strPrev:
+            robo_response = robo_response+"Do you expect a different answer by repeating yourself?"
+        else:
+            if "what" in user_response or "who" in user_response or "how" in user_response:
+                robo_response = robo_response+"I don't know, sorry!"
+            else:
+                robo_response = robo_response+"I am sorry! I don't understand you"
+    else:
+        if user_response == global_strPrev:
+            robo_response += "I've told you before: "
+        robo_response = robo_response+"I Know " + sent_tokens[idx]
+    global_strPrev = user_response
     return robo_response
         
 flag=True
@@ -77,6 +98,12 @@ while(flag==True):
     else:
         flag=False
         print("ROBO: Bye! take care..")
+        
+# current warning:
+# d:\Python38-32\lib\site-packages\sklearn\feature_extraction\text.py:383: UserWarning: 
+# Your stop_words may be inconsistent with your preprocessing. 
+# Tokenizing the stop words generated tokens ['ha', 'le', 'u', 'wa'] not in stop_words.
+#   warnings.warn('Your stop_words may be inconsistent with '
         
 
 # only rare word will be looked in the sentence, and the bots will find for sentence containing them and display them

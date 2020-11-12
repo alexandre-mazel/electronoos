18b4d2905202fd75855be645eb4415141620a586
Alexander Mazel <amazel@aldebaran.com>
2020-11-12 15:03:51 +0100

    ramping added

--- pylepton3/cv2_lepton3.py
+++ pylepton3/cv2_lepton3.py
@@ -50,8 +50,44 @@ def renderCross(im, pos, color, nSize = 2 ):
         im[y+0,x-i] = color
         
 def putTextAndCross( im, pos, color, strText, nSize = 2 ):
-    renderCross(im, pos, color, rTemp, nSize)
-    putTextCentered( im, strText, (pos[0],pos[1]-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2 )
+    renderCross(im, pos, color, nSize)
+    putTextCentered( im, strText, (pos[0],pos[1]-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2 )
+    
+    
+def pix2Temp( v ):
+    """
+    Convert pixel data to temp in Celsius
+    """
+    #~ rT =  ( 0.0217 * (v - 8192) )
+    
+    # with TLinear mode enabled
+    #~ rT = v / 100. - 273.15
+    #~ rT = v / 100.
+    
+    #~ nOffsetK = 273.15
+    #~ nCameraTemperatureScale100 = 30400+780 # + 390 pour le mode maison ou 
+
+    #~ rT = ( 0.0217 * (v - 8192) ) + (nCameraTemperatureScale100/100.) - nOffsetK
+    
+    """
+    mesure:
+    mode maison / humain
+    9447: 71.4
+    9326: 64
+    7882: 23.6
+    7733:   15.4
+    7200: -18
+    7430: 8
+    8530: 41.8
+    8105: 27.7
+    
+    8237: 33.5 / 36.4
+    """
+    # regression estimation
+    a = 0.0346090322
+    b = -254.4290432
+    rT = v*a+b
+    return rT
 
 
 cap = cv2.VideoCapture(2) #or 0 + cv2.CAP_DSHOW
@@ -98,9 +134,12 @@ while(True):
     
     #~ render = cv2.equalizeHist(render) #work only on 8bits
     
-    #~ render = cv2.applyColorMap(render, cv2.COLORMAP_JET) # only for 8bits
     
-    render = cv2.resize(render, None, fx=4, fy=4 )
+    
+    nZoom = 1
+    nZoom = 4 ; render = cv2.resize(render, None, fx=nZoom, fy=nZoom )
+    render = (render/256).astype('uint8')
+    render = cv2.applyColorMap(render, cv2.COLORMAP_JET) # only for 8bits
 
     if 0:
         fn = misctools.getFilenameFromTime() + ".jpg"
@@ -110,21 +149,26 @@ while(True):
         print("INF: output to '%s'" % fn )
         
         
-    if 0:
+    if 1:
         # print temperature on screen:
         a = frame
-        h,w,n = a.shape
+        h,w = a.shape[:2]
         #~ mint = int(a.min()) # use int to copy the variable instead of pointing in the array
         #~ maxt = int(a.max())
         idx_max = np.argmax(a)
-        x_max, y_max = idx_max%w,idx_max/w
+        x_max, y_max = idx_max%w,idx_max//w
         idx_min = np.argmin(a)
-        x_min, y_min = idx_min%w,idx_min/w
-        mint = int(a[y_min,x_min])
-        maxt = int(a[y_max,x_max])
-        centert = int(a[h/2,w/2])
-        txt = str(a[y_min,x_min])
-        putTextAndCross( a, (x_min, y_min), (255,0,0), txt )
+        x_min, y_min = idx_min%w,idx_min//w
+        t_min = int(a[y_min,x_min])
+        t_max = int(a[y_max,x_max])
+        x_center, y_center = w//2,h//2
+        t_center = int(a[y_center,x_center])
+        txt = "%5.1fC" % pix2Temp(t_min)
+        putTextAndCross( render, (x_min*nZoom, y_min*nZoom), (255,0,0), txt )
+        txt = "%s/%5.1fC" % (t_max, pix2Temp(t_max) )
+        putTextAndCross( render, (x_max*nZoom, y_max*nZoom), (0,0,255), txt )
+        txt = "%s/%5.1fC" % (t_center, pix2Temp(t_center) )
+        putTextAndCross( render, (x_center*nZoom, y_center*nZoom), (200,200,200), txt, nSize=4 )
 
 
     # Display the resulting frame

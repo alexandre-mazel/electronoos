0a37881bfc0b40c2cba74b8e6a0535f34224ae40
Alexander Mazel <amazel@aldebaran.com>
2020-12-13 15:07:00 +0100

    sound

---
+++ alex_pytools/sound_processing.py
@@ -0,0 +1,13 @@
+"""
+process big wav file, cut them...
+"""
+import wav
+
+def autocut(wavfile):
+    w = wav.Wav(wavfile)
+    
+    
+if __name__ == "__main__":
+    autocut("C:/Users/amazel/perso/docs/2020-10-10_-_Ravir/robot1.wav")
+    
+    

7194ab8b52e9a647af57eabcd4ec88767d5cad75
Alexander Mazel <alexandre.zelma@gmail.com>
2021-02-01 21:30:24 +0100

    up

--- pepper_scripts/rapid_move.py
+++ pepper_scripts/rapid_move.py
@@ -7,16 +7,22 @@ mem = naoqi.ALProxy("ALMemory", "localhost", 9559)
 rAngleCommand = 0.1
 strJointName = "HipPitch"
 
-#~ rAngleCommand = 0.4
-#~ strJointName = "HeadYaw"
+rAngleCommand = 0.4
+strJointName = "HeadYaw"
 while 1:
     rAngleInit = mem.getData("Device/SubDeviceList/%s/Position/Sensor/Value" % strJointName)
     print("rAngleInit: %5.5f" % rAngleInit)
+    
     rAngleCommand = -rAngleCommand
     print( "%s: start - moveto %s\n" % (time.time(),rAngleCommand) )
     timeBegin = time.time()
-    #~ mot.setAngles(strJointName, rAngleCommand, 0.4)
-    mot.angleInterpolationWithSpeed(strJointName, rAngleCommand, 0.4)
+    
+    rThresholdAngleArrived = 0.02
+    if 1:
+        #~ rThresholdAngleArrived = 0.2 # less precise when using interpolation
+        mot.post.angleInterpolationWithSpeed(strJointName, rAngleCommand, 0.4) # time to start: headyaw ~ 0.0030s whatever the fraction # however it seems far more at usage, weird...
+    else:
+        mot.setAngles(strJointName, rAngleCommand, 0.4)    # time to start: headyaw ~ 0.0020s whatever the fraction
     
     if 0:
         # draw all values
@@ -34,10 +40,10 @@ while 1:
             if not bStartMove:
                 if abs(rAngle - rAngleInit)<0.01:
                     bStartMove = True
-                    print("Start to move: %5.4fs (angle: %5.5f)" % (time.time()-timeBegin,rAngle)) # headyaw ~ 0.002s quelque soit la vitesse
+                    print("Start to move: %5.4fs (angle: %5.5f)" % (time.time()-timeBegin,rAngle))
                     timeStartMove = time.time()
             else:
-                if abs(rAngle - rAngleCommand)<0.03:
+                if abs(rAngle - rAngleCommand)<rThresholdAngleArrived:
                     print("~complete: total: %5.4fs, move: %5.4fs\n" % (time.time()-timeBegin,time.time()-timeStartMove))
                     break
 

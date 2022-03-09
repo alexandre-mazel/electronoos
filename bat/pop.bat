9dfc6b35244c14712ef4dcfb138c38920f2a5771
Alexander Mazel <amazel@aldebaran.com>
2022-03-09 11:18:19 +0100

    up

--- bat/pop.bat
+++ bat/pop.bat
@@ -1,3 +1,13 @@
-@echo off
-FOR /F "tokens=*" %%g IN ('cd') do (SET POP_DIR=%%g)
-echo|set /p="- Current path stored -"
+@ echo off
+rem cf push.bat
+IF "%1"=="" (
+  @cd /D %PUSH_DIR%
+) ELSE (
+rem try to enable a "pop 2 "that will jump to POP_DIR2, works but the cd doesnt execute!
+
+  setlocal enableDelayedExpansion
+  set varname=PUSH_DIR%1
+  set pathdir=!%varname%!
+rem endlocal enableDelayedExpansion
+  call changedir %pathdir%
+)
--- bat/push.bat
+++ bat/push.bat
@@ -1,2 +1,3 @@
-@ rem cf pop.bat
-@cd %POP_DIR%
+@echo off
+FOR /F "tokens=*" %%g IN ('cd') do (SET PUSH_DIR%1=%%g)
+echo|set /p="- Current path stored -"

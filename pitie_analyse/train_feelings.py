53d71f2bcc44a85dee7597269ac1307acce1e653
Alexander Mazel <amazel@aldebaran.com>
2022-09-01 19:30:58 +0200

    up

--- pitie_analyse/train_feelings.py
+++ pitie_analyse/train_feelings.py
@@ -25,7 +25,12 @@ dataset.head()
 reviews = dataset['review'].values.tolist()
 sentiments = dataset['polarity'].values.tolist()
 
+import sklearn
+import sklearn.svm
+import joblib # pip install joblib
+
 testPola = [
+    "c'est top", 1.,
     "c'est super", 1.,
     "c'est très bien", 0.8,
     "c'est bien", 0.6,
@@ -48,3 +53,74 @@ testPola = [
     "pire y'a pas", -1.,
     "si il s'agissait d'un concours du plus nul, il gagnerait", -1,
 ]
+
+
+import torch
+camembert = torch.hub.load('pytorch/fairseq', 'camembert')
+camembert.eval()  # disable dropout (or leave in train mode to finetune)
+
+def explore():
+    aLine= [
+                    "J'aime le camembert !",
+                    "J'aime le nougat !",
+                    "J'aime le nougat et le camembert !",
+                    "J'aime le nougat et le camembert!",
+                    ]
+                    
+
+    for line in aLine:
+        tokens = camembert.encode(line)
+        print("tokens:\n" + str(tokens))
+        last_layer_features = camembert.extract_features(tokens)
+        print(str(last_layer_features))
+        print(len(last_layer_features[0][0]))
+        #~ assert last_layer_features.size() == torch.Size([1, 10, 768]) # en tout cas pour le premier
+    
+    #~ print(dir(tokens))
+    
+def txtToFeats( txt, bVerbose=False ):
+    tokens = camembert.encode(txt)
+    if bVerbose: print("tokens:\n" + str(tokens))
+    last_layer_features = camembert.extract_features(tokens)    
+    if bVerbose: print("last_layer_features:\n" + str(last_layer_features))
+    if bVerbose: print("len[0][0]: %d" % len(last_layer_features[0][0]))
+    if bVerbose: print("len[0][1]: %d" % len(last_layer_features[0][1]))
+    feats = last_layer_features[0][0].detach().numpy()
+    return feats
+
+        
+def train():
+    listFeats = []
+    listPolas = []
+    i = 0
+    while i < len(testPola):
+        txt = testPola[i]; i += 1
+        pola = testPola[i]; i += 1
+        feats = txtToFeats(txt,True)
+        listFeats.append(feats)
+        listPolas.append(int((pola+1)*100))
+        
+    classifierPola = sklearn.svm.SVC(gamma='scale', kernel='rbf', C=17, class_weight='balanced', probability=False)
+    print("INF: train: learning...")
+    classifierPola.fit(listFeats, listPolas)
+    joblib.dump(classifierPola, 'models/clf_pola.pkl')
+    
+def test():
+    classifierPola = joblib.load('models/clf_pola.pkl')
+    listTest = [
+            "alexandre est super",
+            "alexandre est nul",
+            "jean-pierre est super",
+            "jean-pierre est nul",
+            "le cinéma c'est super",
+            "le cinéma c'est top",
+            "le cinéma c'est nul",
+            ]
+    for txt in listTest:
+        feats = txtToFeats(txt)
+        predicted = classifierPola.predict([feats])
+        print("%s => %s, %.2f" % (txt,predicted,(predicted[0]/100.)-1))
+    
+#~ explore()
+train()
+test()

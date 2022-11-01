05e5f917410a255616718a067a1e9bff1df5bfec
Alexander Mazel <amazel@aldebaran.com>
2022-11-01 16:48:27 +0100

    up

--- scripts/guitar_tuner.py
+++ scripts/guitar_tuner.py
@@ -1,5 +1,18 @@
+import sounddevice as sd
 import numpy as np
+import scipy.fftpack
+import os
 
+# General settings
+SAMPLE_FREQ = 44100 # sample frequency in Hz
+WINDOW_SIZE = 44100 # window size of the DFT in samples
+WINDOW_STEP = 21050 # step size of window
+WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ # length of the window in seconds
+SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ # length between two samples in seconds
+windowSamples = [0 for _ in range(WINDOW_SIZE)]
+
+# This function finds the closest note for a given pitch
+# Returns: note (e.g. A4, G#3, ..), pitch of the tone
 CONCERT_PITCH = 440
 ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 def find_closest_note(pitch):
@@ -10,3 +23,36 @@ def find_closest_note(pitch):
   
   
 #~ print(find_closest_note(441)) # => A4 440
+
+# The sounddecive callback function
+# Provides us with new data once WINDOW_STEP samples have been fetched
+def callback(indata, frames, time, status):
+  global windowSamples
+  if status:
+    print(status)
+  if any(indata):
+    windowSamples = np.concatenate((windowSamples,indata[:, 0])) # append new samples
+    windowSamples = windowSamples[len(indata[:, 0]):] # remove old samples
+    magnitudeSpec = abs( scipy.fftpack.fft(windowSamples)[:len(windowSamples)//2] )
+
+    for i in range(int(62/(SAMPLE_FREQ/WINDOW_SIZE))):
+      magnitudeSpec[i] = 0 #suppress mains hum
+
+    maxInd = np.argmax(magnitudeSpec)
+    maxFreq = maxInd * (SAMPLE_FREQ/WINDOW_SIZE)
+    closestNote, closestPitch = find_closest_note(maxFreq)
+
+    os.system('cls' if os.name=='nt' else 'clear')
+    print(f"Closest note: {closestNote} {maxFreq:.1f}/{closestPitch:.1f}")
+  else:
+    print('no input')
+
+# Start the microphone input stream
+try:
+  with sd.InputStream(channels=1, callback=callback,
+    blocksize=WINDOW_STEP,
+    samplerate=SAMPLE_FREQ):
+    while True:
+      pass
+except Exception as e:
+    print(str(e))

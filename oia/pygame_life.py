80d1afc51487a27e456949f3fdf5ad5ca260e7f5
Alexander Mazel <amazel@aldebaran.com>
2022-11-17 15:17:16 +0100

    up

--- oia/pygame_life.py
+++ oia/pygame_life.py
@@ -1,3 +1,4 @@
+import copy
 import pygame
 
 successes, failures = pygame.init()
@@ -5,6 +6,7 @@ print("INF: pygame int: %s successes and %s failure(s)" % (successes, failures))
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+grey = (127, 127, 127)
 red = (255, 0, 0)
 green = (0, 255, 0) 
 blue = (0, 0, 255)
@@ -17,10 +19,17 @@ class Game:
         
         # a game pixel
         self.pixel_img = pygame.Surface((16, 16))
-        self.pixel_img.fill(white)
+        self.pixel_img.fill(grey)
+        
+        # a game pixel lighen
+        self.pixel_img_lighten = pygame.Surface((16, 16))
+        self.pixel_img_lighten.fill(white)
         
         self.w = 40
-        self.h = 20
+        self.h = 26
+        self.renderworld_x = 20 # left top corner when we start to draw our world
+        self.renderworld_y = 20
+        
         self.world = [] # a world of w x h size (not optimal: stored as a standard python list and not numpy array)
         # 0: empty
         # 1: filled
@@ -30,14 +39,48 @@ class Game:
             for i in range(self.w):
                 self.world[j].append(0)
         
+        self.world[self.h//2][self.w//2] = 1 # allume un point central
+
+        for j in range(8):
+            for i in range(8):
+                self.world[j+4][i+4] = 1
+        
         self.keypressed={}
+        self.mousepressed = False
+        
+        
+        
+        self.numframe = 0
+        self.bPause = 0
+        
+    def updateLife( self ):
+        old = copy.deepcopy(self.world) # create a duplication of the world
+        
+        for j in range(1,self.h-1):
+            for i in range(1,self.w-1):
+                count_neighbours =      old[j-1][i-1] \
+                                                + old[j-1][i+0] \
+                                                + old[j-1][i+1] \
+                                                + old[j+0][i-1] \
+                                                \
+                                                + old[j+0][i+1] \
+                                                + old[j+1][i-1] \
+                                                + old[j+1][i+0] \
+                                                + old[j+1][i+1]
+                if count_neighbours == 3:
+                    self.world[j][i] = 1
+                else:
+                    self.world[j][i] = 0
     
-    def update(self):
+    def update( self ):
         """
         return True if user want to quit
         """
         
-        self.updateLife()
+        self.numframe += 1
+        
+        if self.numframe % 50 == 0 and not self.bPause:
+            self.updateLife()
         
         self.clock.tick(self.fps)
 
@@ -60,10 +103,30 @@ class Game:
             if event.type == pygame.KEYUP:
                 self.keypressed[event.key] = 0
                 
+                # ici les touches a  usage unique
+                if event.key == pygame.K_p:
+                    self.bPause = not self.bPause
+                    print("bPause: %s" % self.bPause )
+            
             for key, bPressed in self.keypressed.items():
                 if bPressed:
                     if key == pygame.K_ESCAPE:
                         return True
+                        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.mousepressed = True
+                
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.mousepressed = False
+                
+            if self.mousepressed:
+                pos = pygame.mouse.get_pos()
+                #~ print(pos)
+                wpix, hpix = self.pixel_img.get_size()
+                xgame = (pos[0] - self.renderworld_x)//(wpix+1)
+                ygame = (pos[1] - self.renderworld_y)//(hpix+1)
+                if xgame >= 0 and xgame < self.w and ygame >= 0 and ygame < self.h:
+                    self.world[ygame][xgame] = 1
                     
                     
         return False
@@ -73,9 +136,12 @@ class Game:
         wpix, hpix = self.pixel_img.get_size()
         for j in range(self.h):
             for i in range(self.w):
-                x = 20+i*(wpix+1)
-                y = 20+j*(hpix+1)
-                self.screen.blit(self.pixel_img, [x,y,x+wpix,y+hpix] )
+                x = self.renderworld_x+i*(wpix+1)
+                y = self.renderworld_y+j*(hpix+1)
+                if self.world[j][i] == 1:
+                    self.screen.blit(self.pixel_img_lighten, [x,y,x+wpix,y+hpix] )
+                else:
+                    self.screen.blit(self.pixel_img, [x,y,x+wpix,y+hpix] )
         
         pygame.display.update()  # Or pygame.display.flip()
         

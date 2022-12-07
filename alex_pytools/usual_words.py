c00d1d5ed66e766037d7aa41713d394fc6bd4010
Alexander Mazel <amazel@aldebaran.com>
2022-12-07 21:26:08 +0100

    words up

--- alex_pytools/usual_words.py
+++ alex_pytools/usual_words.py
@@ -3,10 +3,24 @@
 import io
 import json
 
-with io.open('datas/words_frequency_fr.json', encoding="utf8") as dataFile:
-    data = dataFile.read()
-    #~ obj = data[data.find('{') : data.rfind('}')+1]
-    jsonObj = json.loads(data)
-    print(jsonObj)
-    for obj in jsonObj[:20]:
-        print("%s: %d" % (obj["label"],obj["frequency"]))
+class UsualWords:
+    def __init__( self ):
+        self.words = {} # words => occ
+        self.maxOcc = 0 # le mot avec la plus grosse occ
+        
+    def load( self ):
+        with io.open('datas/words_frequency_fr.json', encoding="cp1252") as dataFile:
+            data = dataFile.read()
+            #~ obj = data[data.find('{') : data.rfind('}')+1]
+            jsonObj = json.loads(data)
+            #~ print(jsonObj)
+            self.maxOcc = jsonObj[0]["frequency"]
+            for cpt,obj in enumerate(jsonObj):
+                w = obj["label"]
+                occ = obj["frequency"]
+                self.words[w] = occ
+                if cpt < 20:
+                    print("%s: %d" % (w,occ))
+# class UsualWords - end
+usualWords = UsualWords()
+usualWords.load()

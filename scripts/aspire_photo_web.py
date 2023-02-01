b52f3b4bf543230e7377c6bed66117a964560e15
Alexander Mazel <amazel@aldebaran.com>
2023-02-01 11:30:13 +0100

    up

--- scripts/aspire_photo_web.py
+++ scripts/aspire_photo_web.py
@@ -1,35 +1,53 @@
+import os
+import sys
 
 
-example = "../alex_pytools/autotest_data/page_team_example.htm"
+strLocalPath = os.path.dirname(sys.modules[__name__].__file__)
+if strLocalPath == "": strLocalPath = './'
+sys.path.append(strLocalPath+"/../alex_pytools/")
+import nettools
 
-def saveImageFromPage(pagelocalfilename,websitename = ""):
-    f = open(pagelocalfilename,rt)
+def saveImageFromPage(pagelocalfilename,websiteorigin = "", dst = '/tmp/'):
+    f = open(pagelocalfilename,"rt")
     buf = f.read()
     f.close()
     cpt = 0
     while 1:
-        start = "<img src="
-        idx = buf.search( start )
+        start = '<img src="'
+        idx = buf.find( start )
         if idx == -1:
             break
         buf = buf[idx+len(start):]
-        idx2 = buf.search( '"' )
+        idx2 = buf.find( '"' )
         addr = buf[:idx2]
-        print("addr: %s" % addr)
-        startalt = "<alt="
-        idx = buf.search( startalt )
-        idx0 = buf.search( start ) # ensure it's not the alt of another image
+        print("addr: '%s'" % addr)
+        
+        startalt = 'alt="'
+        idx = buf.find( startalt )
+        idx0 = buf.find( start ) # ensure it's not the alt of another image
+        print("DBG: saveImageFromPage: idx: %d, idx0: %d" % (idx,idx0) )
         if idx0 > idx and idx != -1:
             buf = buf[idx+len(startalt):]
-            idx2 = buf.search( '"' )
+            idx2 = buf.find( '"' )
             alt = buf[:idx2]
-            print("alt: %s" % alt)
+            print("alt: '%s'" % alt)
         else:
             alt = ""
         
+        fndest = os.path.basename(addr)
+        fndest = fndest.split('?')[0]
+        if alt != "":
+            name,ext = os.path.splitext(fndest)
+            fndest = name + "__alt_" + alt + ext
+        fndest = dst + fndest
+        print("Saving to '%s'" % fndest)
+        orig = websiteorigin + addr
+        nettools.download(orig,fndest)
         cpt += 1
-        break
+        #~ break
     print("INF: saveImageFromPage: image found and saved: %s" % cpt )
 # saveImageFromPage - end
 
-saveImageFromPage( pagelocalfilename, websiteorigin = "http://anotherbrain.ai/")
+
+example = "../alex_pytools/autotest_data/page_team_example.htm"
+saveImageFromPage( example, websiteorigin = "http://anotherbrain.ai/")

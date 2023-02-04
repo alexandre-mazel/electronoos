16b8b737740f3a2f486b000f5e0376c0ce1b01dc
Alexander Mazel <amazel@aldebaran.com>
2023-02-04 13:56:20 +0100

    super soft!

---
+++ scripts/search_in_doc.py
@@ -0,0 +1,100 @@
+import docx
+
+import odf
+import odf.opendocument
+
+import os
+import sys
+
+def searchInDocx(filename, s):
+    """
+    search s in filename.
+    return nbr paragraph, nbr match
+    """
+    if ".docx" not in filename:
+        return 0,0
+    s = s.lower()
+    doc = docx.Document(filename)
+    #~ print(dir(doc))
+    #~ print("DBG: nbr paragraph: %s" % len(doc.paragraphs))
+    cpt = 0
+    bFirstTime = 1
+    allparas = doc.paragraphs
+    for nNumPara,p in enumerate(allparas):
+        #~ print("DBG: paragraph: " + p.text)
+        if s in p.text.lower():
+            if bFirstTime:
+                bFirstTime = 0
+                print("***** %s:" % filename)
+            print("paragraph %4d: %s" % ((nNumPara+1),p.text))
+            cpt += 1
+    return len(doc.paragraphs),cpt
+    
+def searchInOdt(filename, s):
+    """
+    search s in filename.
+    return nbr paragraph, nbr match
+    """
+    from odf import text, teletype
+    if ".odt" not in filename:
+        return 0,0
+    s = s.lower()
+    #~ print("odt!!!")
+    doc = odf.opendocument.load(filename)
+    #~ print(dir(doc))
+    allparas = doc.getElementsByType(text.P)
+    #~ print("DBG: nbr paragraph: %s" % len(allparas))
+    cpt = 0
+    bFirstTime = 1
+    for nNumPara,p in enumerate(allparas):
+        p = str(p)
+        #~ print("DBG: paragraph: " + p)
+        if s in p.lower():
+            if bFirstTime:
+                bFirstTime = 0
+                print("***** %s:" % filename)
+            print("paragraph %4d: %s" % ((nNumPara+1),p))
+            cpt += 1
+    return len(allparas),cpt
+    
+def searchInDocs(path,s,bVerbose=0):
+    if path[-1] not in "/\\": path += os.sep
+    listFiles = os.listdir(path)
+    listFiles = sorted(listFiles)
+    cptfilestot = 0
+    cptparatot = 0
+    cptmatchfiles = 0
+    cptmatchlines = 0
+    for f in listFiles:
+        if ".docx" in f:
+            nbrpara,match = searchInDocx( path+f,s)
+        elif ".odt" in f:
+            nbrpara,match = searchInOdt( path+f,s)
+        else:
+            continue
+        cptfilestot += 1
+        cptparatot += nbrpara
+        if match > 0:
+            cptmatchfiles += 1
+            cptmatchlines += match
+
+    return cptfilestot, cptparatot,cptmatchfiles,cptmatchlines
+    
+    
+#~ searchInDoc("C:/tmpdocrufet/1988.docx", "autoroute")
+#~ searchInDocs("C:/tmpdocrufet", "autoroute")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print( "\n  Search in docx/odt v1.0\n\n  syntax: %s <string_to_match>[1]\n\n  eg: %s autoroute \n" % ((sys.argv[0],)*2))
+        exit(-1)
+    print("\n")
+    
+    bVerbose = 0
+    if len(sys.argv)>2 and sys.argv[2]=='1':
+        bVerbose = 1
+    strToMatch = sys.argv[1]
+    print("INF: bVerbose: %s" % bVerbose ) 
+    na,nf,nla,nlm = searchInDocs(".",strToMatch,bVerbose=bVerbose)
+    print("\nNbr Analysed Files: %d\nNbr Matching Files: %d\nNbr Total Paragraph Analysed: %d\nNbr Total Paragraph With Match: %d" % (na,nf,nla,nlm) )
+      

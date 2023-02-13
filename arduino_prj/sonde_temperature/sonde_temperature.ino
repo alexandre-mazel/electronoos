de07a2d68ef77bdb12a0b5d4024a6992392a691b
Alexander Mazel <amazel@aldebaran.com>
2023-02-13 13:52:15 +0100

    work but resistor is MANDATORY!

--- arduino_prj/sonde_temperature/sonde_temperature.ino
+++ arduino_prj/sonde_temperature/sonde_temperature.ino
@@ -1,3 +1,4 @@
+#define DS18B20MODEL 0x28
 #include "OneWire.h"
 #include "DallasTemperature.h"
  
@@ -16,12 +17,102 @@ enum DS18B20_RCODES {
 //OneWire oneWire(10);
 OneWire ds(BROCHE_ONEWIRE);
 
-//DallasTemperature ds(&oneWire);
+//#define USE_DALLAS 1
 
+#ifdef USE_DALLAS
+DallasTemperature sensors(&ds);
+#endif
+
+void discoverOneWireDevices(void) {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  
+  Serial.print("Looking for 1-Wire devices...\n\r");
+
+  while(ds.search(addr)) {
+    Serial.print("\n\rFound \'1-Wire\' device with address:\n\r");
+
+    for( i = 0; i < 8; i++) {
+      Serial.print("0x");
+      if (addr[i] < 16) {
+        Serial.print('0');
+
+      }
+      Serial.print(addr[i], HEX);
+      if (i < 7) {
+        Serial.print(", ");
+
+      }
+    }
+    if ( OneWire::crc8( addr, 7) != addr[7]) {
+        Serial.print("CRC is not valid!\n");
+        return;
+    }
+  }
+  Serial.print("\n\r\n\rThat's it.\r\n");
+  ds.reset_search();
+  return;
+}
+
+void lookUpSensors(){
+  byte address[8];
+  int i=0;
+  byte ok = 0, tmp = 0;
+  //start the search
+  Serial.println("--Search started--");
+  while (ds.search(address)){
+    tmp = 0;
+    //0x10 = DS18S20
+    if (address[0] == 0x10){
+      Serial.print("Device is a DS18S20 : ");
+      tmp = 1;
+    } else {
+      //0x28 = DS18B20
+      if (address[0] == 0x28){
+        Serial.print("Device is a DS18B20 : ");
+        tmp = 1;
+      }
+    }
+    //display the address, if tmp is ok
+    if (tmp == 1){
+      if (OneWire::crc8(address, 7) != address[7]){
+        Serial.println("but it doesn't have a valid CRC!");
+      } else {
+        //all is ok, display it
+        for (i=0;i<8;i++){
+          if (address[i] < 9){
+            Serial.print("0");
+          }
+          Serial.print(address[i],HEX);
+          if (i<7){
+            Serial.print("-");
+          }
+        }
+        Serial.println("");
+        ok = 1;
+      }
+    }//end if tmp
+  }//end while
+  if (ok == 0){
+    Serial.println("No devices were found");
+  }
+  Serial.println("--Search ended--");
+}
 
 void setup() {
   Serial.begin(9600);  // definition de l'ouverture du port serie
-  //ds.begin();          // sonde activee
+  //pinMode(BROCHE_ONEWIRE,INPUT);
+
+#ifdef USE_DALLAS
+  Serial.println("using Dallas");
+  sensors.begin();          // sonde activee
+#else
+  lookUpSensors();
+  //discoverOneWireDevices();
+#endif
+
 }
 
 /**
@@ -32,26 +123,36 @@ byte getTemperature(float *temperature, byte reset_search) {
   // data[] : Données lues depuis le scratchpad
   // addr[] : Adresse du module 1-Wire détecté
   
-  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  /* Reset le bus 1-Wire si nécessaire (requis pour la lecture du premier capteur) */
   if (reset_search) {
+    Serial.println("INF: getTemperature: resetting..." );
     ds.reset_search();
   }
  
   /* Recherche le prochain capteur 1-Wire disponible */
   if (!ds.search(addr)) {
     // Pas de capteur
+    Serial.println("ERR: getTemperature: NO_SENSOR_FOUND" );
     return NO_SENSOR_FOUND;
   }
-  
+
+  Serial.print("addr: ");
+
+  for(int i = 0; i < 9; i++)
+    Serial.print(addr[i]);
+  Serial.println("");
+
   /* Vérifie que l'adresse a été correctement reçue */
   if (OneWire::crc8(addr, 7) != addr[7]) {
     // Adresse invalide
+    Serial.println("ERR: getTemperature: INVALID_ADDRESS" );
     return INVALID_ADDRESS;
   }
  
   /* Vérifie qu'il s'agit bien d'un DS18B20 */
   if (addr[0] != 0x28) {
     // Mauvais type de capteur
+    Serial.println("ERR: getTemperature: INVALID_SENSOR" );
     return INVALID_SENSOR;
   }
  
@@ -81,15 +182,16 @@ byte getTemperature(float *temperature, byte reset_search) {
 }
 
 void loop() {
-  if( 0 )
-  {
-    //ds.requestTemperatures();
-    //int t = ds.getTempCByIndex(0);
-    //Serial.print(t);
-    Serial.println( "C" );
-    delay(1000);
-  }
-  else if( 1)
+#if USE_DALLAS
+
+  sensors.requestTemperatures();
+  int t = sensors.getTempCByIndex(0);
+  Serial.print(t);
+  Serial.println( "C" );
+  delay(1000);
+
+#else
+  if( 1)
   {
     delay(1000);
     float temperature;
@@ -115,5 +217,6 @@ void loop() {
     Serial.println( v );
     delay(10);
   }
+#endif // else DALLAS
 
 }

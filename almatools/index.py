38f6f7dce34e267b15d4d11aed3f30978d7ac485
Alexandre Mazel <alexandre.zelma@gmail.com>
2023-05-31 15:53:17 +0200

    up index at

--- almatools/index.py
+++ almatools/index.py
@@ -4,18 +4,30 @@ def index(req):
     o = """<!DOCTYPE html><html>
     <head><meta charset='cp1252'/>
     <title>AlmaTools EI</title>
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="shortcut icon" href="icon_at.png">
     <style>
     .divelem{
     font-size:20px;
-    border:1px solid #333;
+    border:0px solid #333;
     }
     .imgelem{
     width:320px;
     }
+    .txtelem{
+    padding:10px;
+    }
     </style>
+    
+    <script>
+    function viewPdf(filename)
+    {
+        let e = document.getElementById('pdf_viewer');
+        e.innerHTML = "<embed width=100% height=90% src='"+filename+"'#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0' type='application/pdf'>"
+    }
+    </script>
+    
     </head>
-    <body>
+    <body style="font-family: sans-serif">
     <br>
     <center>
     <img src='logo_at.png' width=300>
@@ -28,22 +40,32 @@ def index(req):
     <br>
     <br>
     <br>
-    <div style='display:flex;flex-direction: row;justify-content:space-around;border:1px solid #333;width:100%'>
+    <br>
+    <br>
+    <div style='display:flex;flex-direction: row;justify-content:space-evenly;border:0px solid #333;width:100%'>
         <div class='divelem'>
-            <img class=imgelem src="logo_at.png"><br>
-            Robotique et embarqué
+            <img class=imgelem src="illus_robotics.png"><br>
+            <div class=txtelem>Robotique et embarqué</div>
         </div>
         <div  class='divelem'>
-            <img class=imgelem src="embed"><br>
-            Agent virtuel, Jeu vidéo et<br>Intelligence Artificielle
+            <img class=imgelem src="illus_agent.png"><br>
+            <div class=txtelem>Agent virtuel, Jeu vidéo et<br>Intelligence Artificielle</div>
         </div>
         <div class='divelem'>
-            <img class=imgelem src="embed"><br>
-            Site web et serveur
+            <img class=imgelem src="illus_web.png"><br>
+            <div class=txtelem>Site web et serveur</div>
         </div>
     </div>
     <br>
-    <a href='tarif.htm'>tarifs</a>
+    <br>
+    <a href='tarifs_almatools.pdf'>Tarifs</a>
+    <!--<a href="javascript:viewPdf('tarifs_almatools.pdf')">Tarifs</a>-->
+    <div id='pdf_viewer'></div>
+    <br>
+    <br>
+    <div style='vertical-align: middle;'>
+    <img src='adr_email.svg' height=15px>
+    </div>
     </body>
     </html>
     """

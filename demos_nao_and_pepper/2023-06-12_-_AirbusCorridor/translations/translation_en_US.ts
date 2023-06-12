52ac15b00de3e16a6e2d97afeab7c0abc1daa42d
Alexander Mazel <amazel@aldebaran.com>
2023-06-12 14:54:44 +0200

    c'est deja pas mal

---
+++ demos_nao_and_pepper/2023-06-12_-_AirbusCorridor/translations/translation_en_US.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/welcoming/Template_StateMachine/behavior_layer1/speak/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Bonjour, si vous avez besoin d'aide, approchez-vous pour me parler.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonjour, si vous avez besoin d'aide, approchez-vous pour me parler.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/welcoming/Template_StateMachine/behavior_layer1/speak/Say (1)</name>
+        <message>
+            <source>Bonne journée.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonne journée.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/welcoming/behavior_layer1/speak/Say</name>
+        <message>
+            <source>Bonjour, si vous avez besoin d'aide, approchez-vous pour me parler.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonjour, si vous avez besoin d'aide, approchez-vous pour me parler.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/welcoming/behavior_layer1/speak/Say (1)</name>
+        <message>
+            <source>Bonne journée.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonne journée.</translation>
+        </message>
+    </context>
+</TS>

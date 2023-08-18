5745bf558a0240927f087b6072a08473e74d2c71
Alexander Mazel <amazel@aldebaran.com>
2023-08-18 11:04:04 +0200

    add stats

--- meteo/temperature_analyse.py
+++ meteo/temperature_analyse.py
@@ -18,41 +18,62 @@ class HelperStat:
         
         self.strPrevDay = ""
         
-        self.bLastStatWasBreakfast = False # helper to know if we analyse the last measure as a breakfast
+        self.bStatBreakfastDone = False # helper to know if we analyse the measure as been made for breakfast
         
-        self.bLastStatWasLunch = False
+        self.bStatLunchDone = False
         
-        self.bLastStatWasDinner = False
+        self.bStatDinnerDone = False
+        
+        self.bStatWetDayDone = False
+        self.bStatWetNightDone = False
 
 class Stat:
     def __init__( self ):
+        self.nbrDay = 0
         self.min = 999
         self.max = -40
         
-        self.nbrBreakfast = 0 # nbr breakfast  at 8.30 > 15 and no rain breakfast
+        self.nbrBreakfast = 0 # nbr breakfast  at 8.30 >= 15 and no rain breakfast
         
-        self.nbrLunch = 0 # nbr lunch at 13 > 20
+        self.nbrLunch = 0 # nbr lunch at 13 >= 20
         
-        self.nbrDinner = 0 # nbr dinner at 19.30 > 20
+        self.nbrDinner = 0 # nbr dinner at 19.30 >= 20
         
         self.nbrHotNight = 0 # nbr min night >= 20
         self.nbrColdNight = 0 # nbr min night < 13
+        self.nbrWetDay = 0 # day with more than 15min of rain # day is >= 8 and < 22
         self.nbrWetNight = 0 # night with more than 15min of rain
         
     def __str__(self):
         o  = ""
         #~ o += " min: %s, max: %s, hotnight: %s" % (self.min,self.max,self.nbrHotNight)
+        o += "  nbr day: %s\n" % self.nbrDay
         o += "  min: %s\n" % self.min
         o += "  max: %s\n" % self.max
+        o += "  breakfast: %s\n" % self.nbrBreakfast
+        o += "  lunch: %s\n" % self.nbrLunch
+        o += "  dinner: %s\n" % self.nbrDinner
         o += "  cold night: %s\n" % self.nbrColdNight
         o += "  hot night: %s\n" % self.nbrHotNight
+        o += "  wet day: %s\n" % self.nbrWetDay
+        o += "  wet night: %s\n" % self.nbrWetNight
         return o
+        
+def isListIn(word,list):
+    """
+    is a member of list in word
+    """
+    for w in list:
+        if w in word:
+            return True
+    return False
 
 def analyse(strFilename):
     bVerbose = 0
     
     f = open(strFilename,"rb")
     occCity = common.OccCounter()
+    occWeather = common.OccCounter()
     
     dicoHelper = {} # city => HelperStat
     
@@ -81,6 +102,22 @@ def analyse(strFilename):
         nTemp = int(strTemp)
         
         bNight = nHour >= 22 or nHour <= 7
+        bDay = not bNight
+        
+        # analyse conditions
+        strWeather = "?"
+        bRain = 0
+        bSnow = 0
+        if len(datas)>4:
+            strWeather = datas[4].decode()
+            strWeatherLo = strWeather.lower()
+            if isListIn(strWeatherLo, ["pluie", "averse", "bruine", "orage", "cipitation"] ):
+                bRain = 1
+            if isListIn(strWeatherLo, ["neige"] ):
+                bSnow = 1
+        bRainOrSnow = bRain or bSnow
+        occWeather.add(strWeather)
+        
         
         if bVerbose: print("bNight: %s" % bNight)
             
@@ -106,7 +143,7 @@ def analyse(strFilename):
                 if  dicoHelper[strCity].minNight >= 20:
                     dicoStat[key].nbrHotNight += 1
                 if dicoHelper[strCity].minNight < 13:
-                    dicoStat[key].nbrColdNight += 1            
+                    dicoStat[key].nbrColdNight += 1
             
         
         
@@ -121,9 +158,14 @@ def analyse(strFilename):
             dicoHelper[strCity] = HelperStat()
             dicoHelper[strCity].strPrevDay = strDate
             
+            
         #~ print(key)
         if not key in dicoStat:
             dicoStat[key] = Stat()
+            
+            
+        if bNewDay:
+            dicoStat[key].nbrDay += 1
             
                 
         if bNight:
@@ -137,6 +179,36 @@ def analyse(strFilename):
 
         if dicoStat[key].max < nTemp:
            dicoStat[key].max = nTemp
+           
+           
+        if not dicoHelper[strCity].bStatBreakfastDone:
+            if nHour >= 8 and nHour < 9 and nTemp >= 15 and not bRainOrSnow:
+                dicoHelper[strCity].bStatBreakfastDone = 1
+                dicoStat[key].nbrBreakfast += 1
+                
+        if not dicoHelper[strCity].bStatLunchDone:
+            if nHour >= 13 and nHour < 14 and nTemp >= 20 and not bRainOrSnow:
+                dicoHelper[strCity].bStatLunchDone = 1
+                dicoStat[key].nbrLunch += 1
+                
+        if not dicoHelper[strCity].bStatDinnerDone:
+            if nHour >= 19 and nHour < 20 and nTemp >= 20 and not bRainOrSnow:
+                dicoHelper[strCity].bStatDinnerDone = 1
+                dicoStat[key].nbrDinner += 1
+                
+        if bRainOrSnow:
+            print("rain")
+        if bDay and not dicoHelper[strCity].bStatWetDayDone:
+            if bRainOrSnow:
+                dicoHelper[strCity].bStatWetDayDone = 1
+                dicoStat[key].nbrWetDay += 1
+                print("wet")
+
+        if bNight and not dicoHelper[strCity].bStatWetNightDone:
+            if bRainOrSnow:
+                dicoHelper[strCity].bStatWetNightDone = 1
+                dicoStat[key].nbrWetNight += 1
+                
 
             
         nNumLine += 1
@@ -145,6 +217,8 @@ def analyse(strFilename):
             break
     
     occCity.printRes()
+    occWeather.printRes()
+    
     for k,v in dicoStat.items():
         print("%s:\n%s" % (k,str(v)))
     

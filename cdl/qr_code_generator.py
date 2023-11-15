e94ac6a5721671a16bb483ac46a9c376b067ea93
Alexander Mazel <amazel@aldebaran.com>
2023-11-15 15:22:00 +0100

    qr ok

--- cdl/qr_code_generator.py
+++ cdl/qr_code_generator.py
@@ -9,11 +9,12 @@ def generateQR( filename,mailcode ):
     img.save(filename)
     print("SUCCESS: QRCode written to file " + filename )
     print("size: %dx%d (pixel size:%s)"  % (img.width, img.height, img.pixel_size) )
+    print("")
     
 
 datas = {
-                "sticker_qrcode_en_NUM": "mailto:photobooth.cdl+NUM@gmail.com?subject=My picture at the Caves du Louvre&body=Please%20attach%20your%20photo%20to%20this%20email.%0D%0A%0D%0ATips%3A%20Select%20%22large%20image%22%20size%20but%20not%20%22real%20size%22.",
-                "sticker_qrcode_fr_NUM": "mailto:photobooth.cdl+NUM@gmail.com?subject=Ma photo aux Caves du Louvre&body=Attachez votre photo a cet email.\n\nConseil: selectionner une image de grande taille mais pas taille r%e9elle."
+                "sticker_qrcode_en_NUM": "mailto:photobooth.cdl%2BNUM@gmail.com?subject=My picture at the Caves du Louvre&body=Please%20attach%20your%20photo%20to%20this%20email.%0D%0A%0D%0ATips%3A%20Select%20%22large%20image%22%20size%20but%20not%20%22real%20size%22.",
+                "sticker_qrcode_fr_NUM": "mailto:photobooth.cdl%2BNUM@gmail.com?subject=Ma photo aux Caves du Louvre&body=Attachez votre photo a cet email.\n\nConseil: selectionner une image de grande taille mais pas taille r%e9elle."
              }
              
 for k,v in datas.items():

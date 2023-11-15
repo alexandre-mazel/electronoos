8043f16f317709b6e108c2ae7b171feaf06d7237
Alexander Mazel <amazel@aldebaran.com>
2023-11-15 22:04:58 +0100

    ca marche toujours pas

--- arduino_prj/piano_cocktail/piano_cocktail.ino
+++ arduino_prj/piano_cocktail/piano_cocktail.ino
@@ -10,28 +10,81 @@ const int LOADCELL_SCK_PIN = 3;
 //#define DOUT A1
 
 HX711 scale;
+float calibration_factor = 1.;
+
+/*
+long read_average(byte times = 10); // Average 'times' raw readings
+double get_value(byte times = 1); // return read_average(times) - OFFSET
+float get_units(byte times = 1); // return get_value(times) / SCALE;
+void tare(byte times = 10); // OFFSET = read_average(times);
+
+So, get_units() returns (read_average(1) - OFFSET) / SCALE;
+*/
+
 
 void setup() {
   
   Serial.begin(9600);
   //pinMode(resetPin, INPUT);
 
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  //scale.set_scale(calibration_factor);
-  scale.tare();                           // reset weight on scale to 0 grams
 
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  while(!scale.is_ready())
+  {
+    Serial.println("INF: Waiting for HX711...");
+    delay(500);
+  }
+  scale.set_scale(calibration_factor);
+  scale.tare();                           // reset weight on scale to 0 grams
+  delay(500);
+  long zero_factor = scale.read_average();
+  Serial.print("Zero factor: ");
+  Serial.println(zero_factor);
+  delay(500);
 }
 
 void loop() {
-  float weight = scale.get_units();
-  Serial.print("Weight: ");
-  Serial.println(weight);
-
-  //if(digitalRead(resetPin) == 0){
-//    scale.tare();     // reset weight on scale to 0 grams
-  //}
-  scale.power_down();             // put the ADC in sleep mode
-  delay(10);
   
-  scale.power_up(); 
+  
+  if (!scale.is_ready()) 
+  {
+    Serial.println("HX711 not found.");
+  }
+  else
+  {
+    Serial.print("calibration_factor: ");
+    Serial.println(calibration_factor);
+    scale.set_scale(calibration_factor);   
+    if(0)
+    { 
+      // tare and measure in same loop
+      Serial.println("Tare... remove any weights from the scale.");
+      delay(5000);
+      scale.tare();
+      Serial.println("Tare done...");
+      Serial.print("Place a known weight on the scale...");
+      delay(5000);
+    }
+    float reading = scale.get_units(10);
+    Serial.print("Result: ");
+    Serial.println(reading);
+    reading = scale.read_average();
+    Serial.print("Result avg: ");
+    Serial.println(reading);
+    if(1)
+    {
+      Serial.println("press key to change calibration factor");
+      if(Serial.available())
+      {
+        char input = Serial.read();
+        if(input == 'a'){ calibration_factor += 100; }
+        else if(input == 'z'){  calibration_factor -= 100; }
+        else if(input == 's'){  calibration_factor += 10; }
+        else if(input == 'x'){  calibration_factor -= 10; }
+        else if(input == 'd'){  calibration_factor += 1; }
+        else if(input == 'c'){  calibration_factor -= 1; }
+      }
+    }
+  }
+  delay(1000);
 }

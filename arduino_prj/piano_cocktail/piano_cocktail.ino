3e043be3108a04728aad692b7235b2e2e16eafcd
Alexander Mazel <amazel@aldebaran.com>
2023-11-16 12:34:53 +0100

    work fine

--- arduino_prj/piano_cocktail/piano_cocktail.ino
+++ arduino_prj/piano_cocktail/piano_cocktail.ino
@@ -5,7 +5,7 @@
 // HX711 circuit wiring
 
 
-#if 0
+#if 1
 // digital ?
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
@@ -20,7 +20,16 @@ const int LOADCELL_SCK_PIN = A0;
 #endif
 
 HX711 scale;
-float calibration_factor = 207; // when set to 1, it's = read/known value
+// si plus petit, ca surcote un peu (les poids affiché semblent etre plus lourd que la réalité)
+
+// reglage pour la barre de 50:
+
+float calibration_factor = 205; // when set to 1, it's = read/known value // poid de 1kg: 206.9, poid de 20g: 193.68
+// avec 200:
+// poids de gym, l'une a 1041 et l'autre a 1036
+// poids de 20g+10g, entre 29.10 et 29.38
+
+float old_calibration_factor = calibration_factor;
 
 /*
 long read_average(byte times = 10); // Average 'times' raw readings
@@ -44,6 +53,8 @@ void setup() {
     Serial.println("INF: Waiting for HX711...");
     delay(500);
   }
+  Serial.print("calibration_factor: ");
+  Serial.println(calibration_factor);
   scale.set_scale(calibration_factor);
   scale.tare();                           // reset weight on scale to 0 grams
   delay(500);
@@ -62,9 +73,15 @@ void loop() {
   }
   else
   {
-    Serial.print("calibration_factor: ");
-    Serial.println(calibration_factor);
-    scale.set_scale(calibration_factor);   
+    if(old_calibration_factor != calibration_factor)
+    {
+      old_calibration_factor = calibration_factor;
+      Serial.print("new calibration_factor: ");
+      Serial.println(calibration_factor);
+      scale.set_scale(calibration_factor);
+      Serial.println("re-taring...");
+      scale.tare(); 
+    }
     if(0)
     { 
       // tare and measure in same loop
@@ -77,7 +94,10 @@ void loop() {
     }
     float reading = scale.get_units(20);
     Serial.print("Result: ");
-    Serial.println(reading);
+    Serial.print(reading);
+    Serial.print(" => ");
+    Serial.print(int(round(reading)));
+    Serial.println(" g");
     long raw = scale.read_average();
     Serial.print("Result raw avg: ");
     Serial.println(raw);
@@ -93,6 +113,8 @@ void loop() {
         else if(input == 'x'){  calibration_factor -= 10; }
         else if(input == 'd'){  calibration_factor += 1; }
         else if(input == 'c'){  calibration_factor -= 1; }
+        else if(input == 'r'){  calibration_factor = 1; }
+        else if(input == 't'){  Serial.println("re-taring..."); scale.tare();}
       }
     }
   }

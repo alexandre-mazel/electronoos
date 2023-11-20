ad9cf2060ad84301d8974b56731c4b11c8e63360
Alexander Mazel <amazel@aldebaran.com>
2023-11-20 22:37:27 +0100

    typo

--- cdl/qr_code_generator.py
+++ cdl/qr_code_generator.py
@@ -14,7 +14,7 @@ def generateQR( filename,mailcode ):
 
 datas = {
                 "sticker_qrcode_en_NUM": "mailto:photobooth.cdl%2BNUM@gmail.com?subject=My picture at the Caves du Louvre&body=Please%20attach%20your%20photo%20to%20this%20email.%0D%0A%0D%0ATips%3A%20Select%20%22large%20image%22%20size%20but%20not%20%22real%20size%22.",
-                "sticker_qrcode_fr_NUM": "mailto:photobooth.cdl%2BNUM@gmail.com?subject=Ma photo aux Caves du Louvre&body=Attachez votre photo a cet email.\n\nConseil: selectionner une image de grande taille mais pas taille r%e9elle."
+                "sticker_qrcode_fr_NUM": "mailto:photobooth.cdl%2BNUM@gmail.com?subject=Ma photo aux Caves du Louvre&body=Attachez votre photo a cet email.\n\nConseil: selectionner une image de grande taille mais pas taille r%C3%A9elle."
              }
              
 for k,v in datas.items():

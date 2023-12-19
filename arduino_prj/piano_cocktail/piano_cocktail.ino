b3a90fe6ef375df3760a1eb7cb4764fc08fe233f
Alexander Mazel <amazel@aldebaran.com>
2023-12-19 12:21:46 +0100

    receive and send order

--- arduino_prj/piano_cocktail/piano_cocktail.ino
+++ arduino_prj/piano_cocktail/piano_cocktail.ino
@@ -1,6 +1,19 @@
 #include "HX711.h" // install in the library manager the HX711 by Rob Tillart (for cell amplifier)
 #include <LiquidCrystal.h>
 
+# define ASSERT(b) __assert((b),"__FUNC__",__FILE__,__LINE__,"");
+// handle diagnostic informations given by assertion and abort program execution:
+void __assert(int bTest, const char *__func, const char *__file, int __lineno, const char *__sexp) 
+{
+  if(bTest)
+    return
+  Serial.println(__func);
+  Serial.println(__file);
+  Serial.println(__lineno, DEC);
+  Serial.println(__sexp);
+  Serial.flush();
+  abort();
+}
 
 // HX711 circuit wiring
 
@@ -83,6 +96,8 @@ void setup() {
   Serial.println(zero_factor);
   delay(500);
   lcd.clear();
+
+  //handleOrder("#Assemble_10_20_30"); // to test when not connected to the tablet
 }
 
 float last_measured = 0;
@@ -90,28 +105,40 @@ float last_measured = 0;
 
 // ask to verse x grammes
 float target_verse = -1001; // negative when no current
-void verse_quantite(float rGrammes)
+int nCurrentVanne = -1;
+void verse_quantite(float rGrammes,int nNumVanne)
 {
   target_verse = last_measured + rGrammes;
-  Serial.print("INF: verse_quantite: current: ");
+  nCurrentVanne = nNumVanne;
+  Serial.print("INF: verse_quantite: cuve: ");
+  Serial.print(nCurrentVanne);
+  Serial.print(", current: ");
   Serial.print(last_measured);
   Serial.print(", target: ");
   Serial.println(target_verse);
   digitalWrite(VANNE_1_PIN, LOW);
 }
 
+int isTargetDefined()
+{
+  return !(target_verse<-1000);
+}
 int check_if_must_stop_verse()
 {
   // return 0 if not versing, 1 if versing, 2 if done
-  if(target_verse<-1000)
+  if(!isTargetDefined())
   {
     return 0;
   }
   float diff = target_verse-last_measured;
-  Serial.print("INF: check_if_must_stop_verse: diff: ");
+  Serial.print("INF: check_if_must_stop_verse: cuve: ");
+  Serial.print(nCurrentVanne);
+  Serial.print(", diff: ");
   Serial.println(diff);
   lcd.print(" => ");
   lcd.print(diff);
+  lcd.print("   ");
+  lcd.print(nCurrentVanne+1);
   if(diff<1+4) // couramment on prend 5 apres coupure
   {
     digitalWrite(VANNE_1_PIN, HIGH);
@@ -139,8 +166,78 @@ void force_stop_verse()
   target_verse = -1001;
 }
 
+/*
+* retrieve int argument in a string, return the number of argumen set
+* argument are coded in a string separated by a '_' _arg1_arg2_arg3... 
+* eg: toto_10_34_45_10
+*/
+int retrieveIntArguments(const char* s, int * pDstArg, int nNbrArgMax)
+{
+  int nNbrArg = 0;
+  const char* p = s;
+  const char* pstart = NULL;
+  while(*p && nNbrArg < nNbrArgMax )
+  {
+    if(*p=='_')
+    {
+      if( pstart != NULL )
+      {
+        pDstArg[nNbrArg] = atoi(pstart); // atoi s'arrete au premier char non decimal
+        nNbrArg += 1;
+      }
+      pstart = p+1;
+
+    }
+    ++p;
+  }
+  // last remainder:
+  if( pstart != NULL && nNbrArg < nNbrArgMax )
+  {
+    pDstArg[nNbrArg]=atoi(pstart);
+    nNbrArg += 1;
+  }
+  return nNbrArg;
+}
+
 #define LEN_COMMAND_MAX 32
 char lastCommand[LEN_COMMAND_MAX+1] = "\0";
+
+#define LEN_QUEUE_ORDER 5
+int queueOrder[LEN_QUEUE_ORDER*2]; // a list of [numcuve,ml,numcuve,ml,...]
+int nNbrQueueOrder = 0; // nbr data in queue order (nbr of data pair)
+
+// receive order format #string
+int handleOrder( const char * command)
+{
+  if(command[1]=='A' && command[2]=='s')
+  {
+    int args[5];
+    int nNbrArgs = retrieveIntArguments(command,args,5);
+    if(1)
+    {
+      // debug
+      Serial.print("DBG: handleOrder: " );
+      for(int i=0;i<nNbrArgs;++i)
+      {
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.print(args[i]);
+        Serial.print(", ");
+      }
+      Serial.println("");
+    }
+
+    // add to queue
+    ASSERT(nNbrArgs<=LEN_QUEUE_ORDER)
+    for( int i=nNbrArgs-1; i >= 0; --i )
+    {
+      queueOrder[nNbrQueueOrder*2+0] = i;
+      queueOrder[nNbrQueueOrder*2+1] = args[i];
+      ++nNbrQueueOrder;
+    }
+
+  }
+}
 
 int handleSerialCommand()
 {
@@ -171,18 +268,20 @@ int handleSerialCommand()
     lastCommand[ichar] = '\0';
     Serial.print("DBG: handleSerialCommand: new command received: ");
     Serial.println( lastCommand );
+    handleOrder(lastCommand);
     return 1;
   }
 
   return 0;
 }
 
-void sendSerialCommand()
+void sendSerialCommand(const char * msg)
 {
-  Serial.print("##coucou/1/2/3/");
+  Serial.print("##");
+  Serial.println(msg);
   //char buf[32];
   //snprintf
-  Serial.println(millis());
+  //Serial.println(millis());
 }
 
 void loop() {
@@ -215,7 +314,7 @@ void loop() {
     }
     float reading = scale.get_units(2); // chaque mesure en plus, c'est prend 88ms
     last_measured = reading;
-    if(1)
+    if(0)
     {
       Serial.print(millis());
       Serial.print(", weight: ");
@@ -229,11 +328,17 @@ void loop() {
     lcd.print(int(round(reading)));
     lcd.print(" g");
 
-    if(check_if_must_stop_verse()==2)
+    int nResVerse = check_if_must_stop_verse();
+    if(nResVerse==2)
     {
-      sendSerialCommand();
+      sendSerialCommand("End0"); // +nCurrentVanne
       lcd.clear();
     }
+    else if(nResVerse==1)
+    {
+      sendSerialCommand("Fill0"); // +nCurrentVanne
+    }
+    
 
     if(0)
     {
@@ -249,26 +354,42 @@ void loop() {
         char input = Serial.read();
         Serial.print("simple letter received: ");
         Serial.println(input);
-        if(input == 'a'){ calibration_factor += 100; }
-        else if(input == 'z'){  calibration_factor -= 100; }
-        else if(input == 's'){  calibration_factor += 10; }
-        else if(input == 'x'){  calibration_factor -= 10; }
-        else if(input == 'd'){  calibration_factor += 1; }
-        else if(input == 'c'){  calibration_factor -= 1; }
-        else if(input == 'r'){  calibration_factor = 1; }
-        else if(input == 't'){  Serial.println("re-taring..."); scale.tare();}
-        else if(input == 'v'){  digitalWrite(VANNE_1_PIN, LOW); delay(1000); digitalWrite(VANNE_1_PIN, HIGH);	 } // a bit of
-        else if(input == '1'){  verse_quantite(10);	 } // asservissement sur poids
-        else if(input == '2'){  verse_quantite(20);	 }
-        else if(input == '3'){  verse_quantite(50);	 }
-        else if(input == '4'){  verse_quantite(100); }
-        else if(input == '5'){  verse_quantite(200); }
-        else if(input == 'd'){  sendSerialCommand(); } // debug
-        else if(input == 'f'){  force_stop_verse();	 } // force stop verse
+        if(1)
+        {
+          // manual command
+          if(input == 'a'){ calibration_factor += 100; }
+          else if(input == 'z'){  calibration_factor -= 100; }
+          else if(input == 's'){  calibration_factor += 10; }
+          else if(input == 'x'){  calibration_factor -= 10; }
+          else if(input == 'd'){  calibration_factor += 1; }
+          else if(input == 'c'){  calibration_factor -= 1; }
+          else if(input == 'r'){  calibration_factor = 1; }
+          else if(input == 't'){  Serial.println("re-taring..."); scale.tare();}
+          else if(input == 'v'){  digitalWrite(VANNE_1_PIN, LOW); delay(1000); digitalWrite(VANNE_1_PIN, HIGH);	 } // a bit of
+          else if(input == '1'){  verse_quantite(10,0);	 } // asservissement sur poids
+          else if(input == '2'){  verse_quantite(20,0);	 }
+          else if(input == '3'){  verse_quantite(50,0);	 }
+          else if(input == '4'){  verse_quantite(100,0); }
+          else if(input == '5'){  verse_quantite(200,0); }
+          else if(input == 'd'){  sendSerialCommand("Debug/1/2/3"); } // debug
+          else if(input == 'f'){  force_stop_verse();	 } // force stop verse
+        }
         if(handleSerialCommand())
         {
           lcd.setCursor(0, 1);
           lcd.print(lastCommand);
+        }
+      }
+    }
+    if(1)
+    {
+      if( nNbrQueueOrder > 0 )
+      {
+        // handle queue
+        if(!isTargetDefined())
+        {
+          --nNbrQueueOrder;
+          verse_quantite(queueOrder[nNbrQueueOrder*2+1],queueOrder[nNbrQueueOrder*2+0]);
         }
       }
     }

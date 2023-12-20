03d3a91bd850a95b4f50f0be5f93c76f205b239c
Alexander Mazel <amazel@aldebaran.com>
2023-12-20 21:00:46 +0100

    up

---
+++ arduino_prj/solar_manager/solar_manager.ino
@@ -0,0 +1,41 @@
+#include "oled.h"       // **** OLED ****
+
+OLED disp(20,21,NO_RESET_PIN,OLED::W_128,OLED::H_64,OLED::CTRL_SH1106,0x3C);
+
+#define DISP(x,y,z) disp.drawString(x,y,z)
+
+#define PIN_VOLT_MEASURE A0
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Setup start");
+  disp.begin();
+  disp.set_contrast(8); // the eye doesn't register big difference, but power consumptions is
+  disp.set_scrolling(OLED::NO_SCROLLING);
+  Serial.println("Setup end");
+}
+
+void loop()
+{
+  //Serial.println("looping...");
+
+  int nVoltRead = analogRead(PIN_VOLT_MEASURE);
+  float rVoltResult = nVoltRead * 85/10;
+
+  char result[8];
+  char line[16] = "";
+  dtostrf(rVoltResult, 6, 2, result);
+  strcat(line," Puis:");
+  strcat(line, result);
+  strcat(line, "V");
+
+  DISP(0,0,"- Solar Manager -");
+  DISP(0,1,line);
+  DISP(0,7,"Bas de l'ecran");
+  disp.display();
+
+
+  //Serial.println("sleeping...");
+  delay(10);
+}
--- arduino_prj/test_hailege_128_64/test_hailege_128_64.ino
+++ arduino_prj/test_hailege_128_64/test_hailege_128_64.ino
@@ -2,13 +2,14 @@
 
 OLED display(20,21,NO_RESET_PIN,OLED::W_128,OLED::H_64,OLED::CTRL_SH1106,0x3C);
 
-#define DISP(x,y,z) display.drawString(x,y,z);display.display()
+#define DISP(x,y,z) display.drawString(x,y,z)
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Setup start");
   display.begin();
+  display.set_contrast(8); // the eye doesn't register big difference, but power consumptions is
   display.set_scrolling(OLED::NO_SCROLLING);
   Serial.println("Setup end");
 }
@@ -16,13 +17,36 @@ void setup()
 void loop()
 {
   //Serial.println("looping...");
-  for(int i = 0; i < 10; ++i)
+  if(0)
   {
-    display.draw_circle(5+i*10,5,5);
+    // des petits ronds
+    for(int i = 0; i < 10; ++i)
+    {
+      display.draw_circle(7+i*10,5,5); // 7+ car sinon le rond n'est pas entier, pourquoi on peut pas dessiner en 0,0 ?
+      display.draw_pixel(7+i*10,5);
+    }
+    display.display();
   }
-  //DISP(0,0,"start");
-  //DISP(6,8,"Hello World");
-  display.display();
+  if(0)
+  {
+    // full blanc
+    for(int j = 0; j < 64; ++j)
+    {
+      for(int i = 0; i < 128; ++i)
+      {
+        display.draw_pixel(i,j);
+      }
+    }
+    display.display();
+  }
+  if(1)
+  {
+    DISP(0,0,"Start");
+    DISP(0,1,"Autre message");
+    DISP(6,4,"Hello World");
+    display.display();
+  }
+
   //Serial.println("sleeping...");
   delay(10);
 }

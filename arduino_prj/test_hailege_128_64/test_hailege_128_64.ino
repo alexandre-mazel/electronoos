68e6549ffe229faa5e717581dc889a5c62c2a349
Alexander Mazel <amazel@aldebaran.com>
2023-12-20 15:58:34 +0100

    ca fonctionne

--- arduino_prj/test_hailege_128_64/test_hailege_128_64.ino
+++ arduino_prj/test_hailege_128_64/test_hailege_128_64.ino
@@ -1,6 +1,6 @@
 #include "oled.h"       // **** OLED ****
 
-OLED display(20,21,NO_RESET_PIN,OLED::W_128,OLED::H_64,OLED::CTRL_SH1106,0x3D);
+OLED display(20,21,NO_RESET_PIN,OLED::W_128,OLED::H_64,OLED::CTRL_SH1106,0x3C);
 
 #define DISP(x,y,z) display.drawString(x,y,z);display.display()
 
@@ -15,11 +15,14 @@ void setup()
 
 void loop()
 {
-  Serial.println("looping...");
-  //display.draw_circle(4,4,5);
+  //Serial.println("looping...");
+  for(int i = 0; i < 10; ++i)
+  {
+    display.draw_circle(5+i*10,5,5);
+  }
   //DISP(0,0,"start");
-  DISP(6,8,"Hello World");
-  //display.display();
-  Serial.println("sleeping...");
+  //DISP(6,8,"Hello World");
+  display.display();
+  //Serial.println("sleeping...");
   delay(10);
 }

541c599e9d439df6e9cc744c419ba77ae3411c15
Alexander Mazel <amazel@aldebaran.com>
2023-12-20 15:41:40 +0100

    ca fonctionne pas

---
+++ arduino_prj/test_hailege_128_64/test_hailege_128_64.ino
@@ -0,0 +1,25 @@
+#include "oled.h"       // **** OLED ****
+
+OLED display(20,21,NO_RESET_PIN,OLED::W_128,OLED::H_64,OLED::CTRL_SH1106,0x3C);
+
+#define DISP(x,y,z) display.drawString(x,y,z);display.display()
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Setup start");
+  display.begin();
+  display.set_scrolling(OLED::NO_SCROLLING);
+  Serial.println("Setup end");
+}
+
+void loop()
+{
+  Serial.println("looping...");
+  //display.draw_circle(4,4,5);
+  //DISP(0,0,"start");
+  DISP(6,8,"Hello World");
+  //display.display();
+  Serial.println("sleeping...");
+  delay(10);
+}

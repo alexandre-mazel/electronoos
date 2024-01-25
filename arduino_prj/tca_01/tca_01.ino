93d6c4871349eb8c45bd89cd39984ff2c76f2555
Alexander Mazel <amazel@aldebaran.com>
2024-01-25 13:59:53 +0100

    encoder works!

--- arduino_prj/tca_01/tca_01.ino
+++ arduino_prj/tca_01/tca_01.ino
@@ -22,24 +22,51 @@ void loop()
 {
   int val1 = enc1.read();
   int val2 = 0;
-  float rRev = val1/(31.*4);
+  float rRev = val1/(31*4.);
+
+  if(0)
+  {
+    Serial.print("enc1: ");
+    Serial.print(val1);
+    Serial.print(", rev: ");
+    Serial.println(rRev);
+  }
+
 
   if( timeChange <= millis() || (rRev>0. && rRev>=2.) || (rRev<0. && rRev<=-2.) )
   {
     analogWrite(PWM_TWIST, 0);
     delay(500);
-    enc1.write(0);
 
-    timeChange = millis()+5000;
-    analogWrite(PWM_TWIST, 25); // 25 => 10%
+    if(1)
+    {
+      int val1 = enc1.read();
+      float rRev = val1/(31*4.);
+      
+      Serial.print("at stop: enc1: ");
+      Serial.print(val1);
+      Serial.print(", rev: ");
+      Serial.println(rRev);
+    }
+
+
+    enc1.write(0);
+    delay(500);
+
+    timeChange = millis()+10000;
+    analogWrite(PWM_TWIST, 25); // 25 => 10% // en dessous de 12 ca demarre pas toujours
     digitalWrite(PHASE_TWIST, bTwistDir);
-    bTwistDir = !bTwistDir;
+    //bTwistDir = !bTwistDir;
   }
 
-  Serial.print("enc1: ");
-  Serial.print(val1);
-  Serial.print(", rev: ");
-  Serial.println(rRev);
+  delay(10);
 
-  delay(100);
+  // quand on affiche la valeur a l'arret (on lit la meme erreur dans les 2 sens)
+
+  // a pwm 25, avec une loop sans delay, on a 0.00 d'erreur entre la lecture et l'arret.
+  // a pwm 25, avec une loop a 10, on a 0.00 d'erreur entre la lecture et l'arret
+  // a pwm 25, avec une loop a 100, on a 0.02 d'erreur entre la lecture et l'arret
+  
+  // a pwm 255, avec une loop sans delay, on a 0.13 d'erreur entre la lecture et l'arret.
+  // a pwm 255, avec une loop a 100, on a 0.70 d'erreur entre la lecture et l'arret, c'est assez constant.
 }

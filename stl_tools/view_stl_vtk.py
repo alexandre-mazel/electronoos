bc2971ffcf8c4209a3d4226ca698b20c7812ee7d
Alexander Mazel <amazel@aldebaran.com>
2024-02-08 12:10:17 +0100

    nice viewer

--- stl_tools/view_stl_vtk.py
+++ stl_tools/view_stl_vtk.py
@@ -1,9 +1,17 @@
 import vtk # pip install vtk # tested with vtk-9.3.0-cp39-cp39-win_amd64.whl
 
-def main():
+def showStl(filename):
+    
+        
+    def callback_func_animate(caller, timer_event):
+        actor.RotateX(1)
+        actor.RotateY(0.35)
+        actor.RotateZ(0.13)
+        renderWindow.Render()
+        
     # Create a reader
     reader = vtk.vtkSTLReader()
-    reader.SetFileName("MovingWeight_with_roller.stl")
+    reader.SetFileName(filename)
 
     # Create a mapper
     mapper = vtk.vtkPolyDataMapper()
@@ -12,24 +20,43 @@ def main():
     # Create an actor
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
+    
+    #actor.SetPosition(0,0,0)
+    # put the center at the center of the world!
+    center = actor.GetCenter()
+    actor.SetOrigin(center)
+    #~ actor.RotateX(30)
+    #~ actor.RotateY(30)
+    #~ actor.RotateZ(30)
 
     # A renderer and render window
     renderer = vtk.vtkRenderer()
     renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetSize(768,768)
+    renderWindow.SetWindowName(filename)
+
     renderWindow.AddRenderer(renderer)
 
     # An interactor
     renderWindowInteractor = vtk.vtkRenderWindowInteractor()
     renderWindowInteractor.SetRenderWindow(renderWindow)
 
+
     # Add the actors to the scene
     renderer.AddActor(actor)
     rBackgroundColor = 0.75
     renderer.SetBackground(rBackgroundColor, rBackgroundColor, rBackgroundColor)
+    
+    # animate
+    renderWindowInteractor.Initialize()
+    refresh_rate = 25 # ms
+    renderWindowInteractor.CreateRepeatingTimer(int(refresh_rate))
+    renderWindowInteractor.AddObserver("TimerEvent", callback_func_animate)
 
     # Render and interact
     renderWindow.Render()
     renderWindowInteractor.Start()
 
+
 if __name__ == "__main__":
-    main()
+    showStl("MovingWeight_with_roller.stl")

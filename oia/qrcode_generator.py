8d60fc9e4aacee6317aec1f1d73d2ff659c41a84
Alexander Mazel <amazel@aldebaran.com>
2024-03-26 23:10:22 +0100

    add qr code stickers

--- oia/qrcode_generator.py
+++ oia/qrcode_generator.py
@@ -7,15 +7,17 @@ data = "mailto:alexandre.zelma@gmail.com?subject=Location%20Studio%20Sceaux.&bod
 data = "mailto:alexandre.zelma@gmail.com?subject=Location%20Studio%20Sceaux."
 data = "tel:+33610601979"
 data = "https://obo-world.com/presence.htm?c=kendo&b=champs_elysee" # moved to obo/spider
-data = "mailto:photobooth.cdl+1@gmail.com?subject=My picture at the Caves du Louvre&body=Please%20attach%20your%20photo%20to%20this%20email.%0D%0A%0D%0ATips%3A%20Select%20%22large%20image%22%20size%20but%20not%20%22real%20size%22."
-data = "http://obo-world.com/vitrine/index_en.html?src=ces"
-data = "http://obo-world.com/index.html?src=tf"
-data = "http://youtu.be/Xl5sS80VrTM" # video pres sonia fr
-#~ data = "http://obo-world.com/download/almavision.apk"
-#~ data = "https://www.linkedin.com/in/alexandremazel/"
-data = "http://linkedin.com/in/alexandremazel/"
-data = "http://obo-world.com/vitrine/form_candi.htm"
-data = """BEGIN:VCARD
+data = "mailto:photobooth.cdl%2B7@gmail.com?subject=Ma photo aux Caves du Louvre&body=Attachez%20votre%20photo%20%C3%A0%20cet%20email.%0D%0A%0D%0AConseil: s%C3%A9lectionner une image de grande taille, mais pas%20%22taille%20r%C3%A9elle%22."
+#~ data = "mailto:photobooth.cdl%2B7@gmail.com?subject=My picture at the Caves du Louvre&body=Please%20attach%20your%20photo%20to%20this%20email.%0D%0A%0D%0ATips%3A%20Select%20%22large%20image%22%20size%20but%20not%20%22real%20size%22."
+if 0:
+    data = "http://obo-world.com/vitrine/index_en.html?src=ces"
+    data = "http://obo-world.com/index.html?src=tf"
+    data = "http://youtu.be/Xl5sS80VrTM" # video pres sonia fr
+    #~ data = "http://obo-world.com/download/almavision.apk"
+    #~ data = "https://www.linkedin.com/in/alexandremazel/"
+    data = "http://linkedin.com/in/alexandremazel/"
+    data = "http://obo-world.com/vitrine/form_candi.htm"
+    data = """BEGIN:VCARD
 VERSION:3.0
 N:Mazel;Alexandre
 FN:Alexandre Mazel
@@ -23,9 +25,9 @@ ORG:Obo-World;Engrenage.studio;AlmaTools;Freelance
 ROLE:CTO;CEO;CCO
 TITLE:CTO;CEO;CCO
 """
-#~ data += "URL:http://obo-world.com\n"
-data += "URL:http://linkedin.com/in/alexandremazel/\n"
-data += """PHOTO;TYPE=PNG:http://obo-world.com/vitrine/img/alexandre_o.png
+    #~ data += "URL:http://obo-world.com\n"
+    data += "URL:http://linkedin.com/in/alexandremazel/\n"
+    data += """PHOTO;TYPE=PNG:http://obo-world.com/vitrine/img/alexandre_o.png
 EMAIL:alexandre.mazel@obo-world.com
 TEL;TYPE=voice,work,pref:+33 610601979
 END:VCARD

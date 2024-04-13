09b8af88487dd895898dd125a59e648787eba23e
Alexander Mazel <amazel@aldebaran.com>
2024-04-13 11:24:02 +0200

    start rot cylinder

--- stl_tools/generate_stl.py
+++ stl_tools/generate_stl.py
@@ -95,46 +95,73 @@ def generatePara(o, center, size):
     
 # generatePara - end
     
-def generateRing(o, center, hole_radius, disk_w, disk_h, alpha=0, beta=0 ):
+def generateRing(o, center, hole_radius, disk_w, disk_h, rx=0, ry=0, rz = 0, circ = 2*pi ):
     """
     a cylinder with a hole in it.
+    the base is in the Oxy plane (Ox is the seg=0 axis)(ou alors c'est Oy)
     - alpha and beta: angle of the radial axis.
-        - alpha: angle with x (degrees)
-        - beta: angle with y (degrees)
+        - rx: rotation around x axis(degrees)
+        - ry: ...
+        - rz: ... applied first (decay of seg)
+        - circ: put less than 2pi to limit the completness of the ring
     - disk_w: width of disk
     """
     disk_h /= 2 # offset around center
     
-    step = pi/240
+    step = pi/12
     seg = 0 # advancing in the disk
-    while seg < pi*2:
+    
+    rx = rx *pi/180
+    ry = ry *pi/180
+    rz = rz *pi/180
+    
+    while seg < circ:
         d1 = hole_radius
-        d2 = hole_radius+disk_h
-        p1 = center[0]+d1*cos(seg),center[1]+d1*sin(seg),center[2]-disk_h
-        p2 = center[0]+d2*cos(seg),center[1]+d2*sin(seg),center[2]-disk_h
-        p3 = center[0]+d1*cos(seg+step),center[1]+d1*sin(seg+step),center[2]-disk_h
-        p4 = center[0]+d2*cos(seg+step),center[1]+d2*sin(seg+step),center[2]-disk_h
+        d2 = hole_radius+disk_w
+        p1 = center[0]+d1*cos(seg+rz)*cos(rx)*cos(ry)-disk_h*sin(rx), center[1]+d1*sin(seg+rz)*cos(rx)*cos(ry),center[2]-disk_h*sin(rx)*sin(ry)
+        p2 = center[0]+d2*cos(seg+rz)*cos(rx)*cos(ry), center[1]+d2*sin(seg+rz)*cos(rx)*cos(ry),center[2]-disk_h*sin(rx)*sin(ry)
+        p3 = center[0]+d1*cos(seg+rz+step)*cos(rx)*cos(ry), center[1]+d1*sin(seg+rz+step)*cos(rx)*cos(ry),center[2]-disk_h*sin(rx)*sin(ry)
+        p4 = center[0]+d2*cos(seg+rz+step)*cos(rx)*cos(ry), center[1]+d2*sin(seg+rz+step)*cos(rx)*cos(ry),center[2]-disk_h*sin(rx)*sin(ry)
+        
+        #~ p1 = p1[0]*cos(rx)*cos(ry), p1[1]*cos(rx)*cos(ry), p1[2]*sin(rx)*sin(ry)
+        #~ p2 = p2[0]*cos(rx)*cos(ry), p2[1]*cos(rx)*cos(ry), p2[2]*sin(rx)*sin(ry)
+        #~ p3 = p3[0]*cos(rx)*cos(ry), p3[1]*cos(rx)*cos(ry), p3[2]*sin(rx)*sin(ry)
+        #~ p4 = p4[0]*cos(rx)*cos(ry), p4[1]*cos(rx)*cos(ry), p4[2]*sin(rx)*sin(ry)
         
         # base
         o.addQuad(p1,p2,p4,p3)
 
-
-        p5 = center[0]+d1*cos(seg),center[1]+d1*sin(seg),center[2]+disk_h
-        p6 = center[0]+d2*cos(seg),center[1]+d2*sin(seg),center[2]+disk_h
-        p7 = center[0]+d1*cos(seg+step),center[1]+d1*sin(seg+step),center[2]+disk_h
-        p8 = center[0]+d2*cos(seg+step),center[1]+d2*sin(seg+step),center[2]+disk_h
+        p5 = center[0]+d1*cos(seg+rz),center[1]+d1*sin(seg+rz),center[2]+disk_h
+        p6 = center[0]+d2*cos(seg+rz),center[1]+d2*sin(seg+rz),center[2]+disk_h
+        p7 = center[0]+d1*cos(seg+rz+step),center[1]+d1*sin(seg+rz+step),center[2]+disk_h
+        p8 = center[0]+d2*cos(seg+rz+step),center[1]+d2*sin(seg+rz+step),center[2]+disk_h
         
         # top
-        o.addQuad(p5,p6,p8,p7)
+        #~ o.addQuad(p5,p6,p8,p7)
         
         # inner side
-        o.addQuad(p1,p3,p7,p5)
+        #~ o.addQuad(p1,p3,p7,p5)
         
         # outer side
-        o.addQuad(p2,p4,p8,p6)
+        #~ o.addQuad(p2,p4,p8,p6)
+        
+        # outer side
+        #~ o.addQuad(p3,p4,p8,p7)
+        
+            
+        if seg == 0 and circ < 2*pi:
+            # face of first tranche
+            #~ o.addQuad(p1,p2,p6,p5)
+            pass
         
         
         seg += step
+    
+    if circ < 2*pi:
+        # face of last tranche
+        #~ o.addQuad(p3,p4,p8,p7)
+        pass
+        
         
 # generateRing - end
     
@@ -146,7 +173,10 @@ def test():
     #~ generateParaCentered(o)
     #~ generatePara(o,(0,0,5),(10,20,10))
     #~ generatePara(o,(0,0,15),(4,2,10))
-    generateRing(o,(3,5,10),5,10,20)
+    #~ generateRing(o,(3,5,10),5,10,20)
+    #~ generateRing(o,(5,5,32),2,11,23,rz=15,circ=pi*4/3)
+    generateRing(o,(5,5,16),2,11,23,rx=25,circ=pi*21/12)
+    generateRing(o,(0,0,0),2,11,23,rx=45,circ=pi*4/3)
     if 1:
         strFilename = "generated.stl"
         o.saveToStl(strFilename)

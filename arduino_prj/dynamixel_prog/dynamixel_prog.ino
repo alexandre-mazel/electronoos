74e76b066133149d93851b083d77d57de6ff7acb
Alexander Mazel <amazel@aldebaran.com>
2024-04-15 16:22:13 +0200

    ok for slow

--- arduino_prj/dynamixel_prog/dynamixel_prog.ino
+++ arduino_prj/dynamixel_prog/dynamixel_prog.ino
@@ -95,12 +95,12 @@ void setup() {
     dxl.torqueOff(nMotorId);
     dxl.setOperatingMode(nMotorId, OP_VELOCITY);
     dxl.torqueOn(nMotorId);
-    //dxl.setGoalVelocity(nMotorId, 100, UNIT_PERCENT);
-    dxl.setGoalVelocity(nMotorId, 1126, UNIT_RAW);
-    delay(2000);
+    dxl.setGoalVelocity(nMotorId, 10, UNIT_PERCENT);
+    //dxl.setGoalVelocity(nMotorId, 1126, UNIT_RAW); // this line block the upload !!! (nearly at the end timeout)
+    delay(4000);
     //dxl.setGoalVelocity(nMotorId, -10, UNIT_PERCENT); // WRN: bug in the lib for protocol 1 wirh negative values
     dxl.setGoalVelocity(nMotorId, 102, UNIT_RAW); // 50: very slow reverse, 1023: fast reverse, 1074: slow straight, 2047: fast straight
-    delay(2000);
+    delay(4000);
     //dxl.setGoalVelocity(nMotorId, 0, UNIT_PERCENT);
     //delay(2000);
     dxl.torqueOff(nMotorId);

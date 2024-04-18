5b6ab3248ea858a217c9a51e0d7b68201294bda0
Alexander Mazel <amazel@aldebaran.com>
2024-04-18 17:49:36 +0200

    thermostat ok

--- arduino_prj/four_custom/four_custom.ino
+++ arduino_prj/four_custom/four_custom.ino
@@ -1,10 +1,21 @@
 const int analogPin = A3;
+const int chauffePin1 = 30;
+const int chauffePin2 = 31;
 
 const int rThermalResistanceReference = 678.0;
 
 void setup() 
 {
   Serial.begin(57600);           //  setup serial
+
+  pinMode(chauffePin1, OUTPUT);
+  pinMode(chauffePin2, OUTPUT);
+  digitalWrite(chauffePin1, LOW);
+  digitalWrite(chauffePin2, LOW);
+
+  Serial.println("Starting...");
+  delay(1000);
+
 }
 
 float inputToResistance( int nAnalogValue, const float rKnownResistance )
@@ -12,7 +23,7 @@ float inputToResistance( int nAnalogValue, const float rKnownResistance )
   const float rVin = 5.0f;
   float rVoltageMeasured = nAnalogValue*(rVin /1023.0);
   float resistance = rKnownResistance * ( 1/(rVin/rVoltageMeasured-1) );
-  Serial.print("rVin: "); Serial.print(rVin,2); Serial.print(", rVoltageMeasured: "); Serial.print(rVoltageMeasured,2); Serial.print(", resistance: "); Serial.println(resistance,2);
+  Serial.print("nAnalogValue: "); Serial.print(nAnalogValue); Serial.print(", rVin: "); Serial.print(rVin,2); Serial.print(", rVoltageMeasured: "); Serial.print(rVoltageMeasured,2); Serial.print(", resistance: "); Serial.println(resistance,2);
   return resistance;
 }
 
@@ -20,7 +31,30 @@ void loop()
 {
   int val = analogRead(analogPin);
   float resistance = inputToResistance(val, rThermalResistanceReference);
+  float temperature = resistance / 16.;
   Serial.print("resistance: ");
-  Serial.println(resistance,2);
+  Serial.print(resistance,2);
+  Serial.print(", temperature: ");
+  Serial.println(temperature,2);
+  float target = 100;
+  if(temperature < 10 ||temperature > 300 )
+  {
+    Serial.println("EMERGENCY. Turning off!");
+    digitalWrite(chauffePin1, LOW);
+    digitalWrite(chauffePin1, HIGH);
+    delay(5000);
+    return;
+  }
+  if( temperature > target+5 )
+  {
+    digitalWrite(chauffePin1, LOW);
+    Serial.println( "Chauffe STOP");
+
+  }
+  else if( temperature < target -5 )
+  {
+    digitalWrite(chauffePin1, HIGH);
+    Serial.println( "Chauffe START");
+  }
   delay(500);
 }

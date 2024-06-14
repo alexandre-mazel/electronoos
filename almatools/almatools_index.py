adf65c700b2a0b9f796bd90b83023d88f67fabdd
Alex Mazel <alexandre.zelma@gmail.com>
2024-06-14 20:04:08 +0200

    almatools working no and also almatools/

--- almatools/almatools_index.py
+++ almatools/almatools_index.py
@@ -1,10 +1,10 @@
-
+# -*- coding: cp1252 -*-
 
 def index(req):
     o = """<!DOCTYPE html><html>
     <head><meta charset='cp1252'/>
     <title>AlmaTools EI</title>
-    <link rel="shortcut icon" href="icon_at.png">
+    <link rel="shortcut icon" href="/almatools/icon_at.png">
     <style>
     .divelem{
     font-size:20px;
@@ -30,12 +30,12 @@ def index(req):
     <body style="font-family: sans-serif">
     <br>
     <center>
-    <img src='logo_at.png' width=300>
+    <img src='/almatools/logo_at.png' width=300>
     <br>
     <br>
     <br>
     <font size=+3>
-    Prestation de conseil et développement
+    Prestation de conseil et d&eacute;veloppement
     </font>
     <br>
     <br>
@@ -44,27 +44,27 @@ def index(req):
     <br>
     <div style='display:flex;flex-direction: row;justify-content:space-evenly;border:0px solid #333;width:100%'>
         <div class='divelem'>
-            <img class=imgelem src="illus_robotics.png"><br>
-            <div class=txtelem>Robotique et embarqué</div>
+            <img class=imgelem src="/almatools/illus_robotics.png"><br>
+            <div class=txtelem>Robotique et embarqu&eacute;</div>
         </div>
         <div  class='divelem'>
-            <img class=imgelem src="illus_agent.png"><br>
-            <div class=txtelem>Agent virtuel, Jeu vidéo et<br>Intelligence Artificielle</div>
+            <img class=imgelem src="/almatools/illus_agent.png"><br>
+            <div class=txtelem>Agent virtuel, Jeu vid&eacute;o et<br>Intelligence Artificielle</div>
         </div>
         <div class='divelem'>
-            <img class=imgelem src="illus_web.png"><br>
+            <img class=imgelem src="/almatools/illus_web.png"><br>
             <div class=txtelem>Site web et serveur</div>
         </div>
     </div>
     <br>
     <br>
-    <a href='tarifs_almatools.pdf'>Tarifs</a>
-    <!--<a href="javascript:viewPdf('tarifs_almatools.pdf')">Tarifs</a>-->
+    <a href='/almatools/tarifs_almatools.pdf'>Tarifs</a>
+    <!--<a href="javascript:viewPdf('/almatools/tarifs_almatools.pdf')">Tarifs</a>-->
     <div id='pdf_viewer'></div>
     <br>
     <br>
     <div style='vertical-align: middle;'>
-    <img src='adr_email_at.svg' height=15px>
+    <img src='/almatools/adr_email_at.svg' height=15px>
     </div>
     </body>
     </html>

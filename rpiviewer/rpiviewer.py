ea14c40886bfbbc4cf922a6a32b35e333f8695ee
Alexander Mazel <amazel@aldebaran.com>
2024-06-27 17:28:59 +0200

    start

--- rpiviewer/rpiviewer.py
+++ rpiviewer/rpiviewer.py
@@ -2,11 +2,38 @@ from tkinter import *
 from tkinter import ttk
 
 def show_user_settings():
+    """
+    from https://docs.python.org/3/library/tkinter.html
+    and https://python.doctor/page-tkinter-interface-graphique-python-tutoriel
+    """
     root = Tk()
-    frm = ttk.Frame(root, padding=10)
+    frm = ttk.Frame(root, padding=20)
+    # we define a grid of 5 columns
+    colcenter = 2
     frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=10)
+    nNumRow = 0
+    ttk.Label(frm, text="RPIVIEWER",background="lightblue").grid(column=colcenter, row=nNumRow); nNumRow += 1
+    ttk.Label(frm, text="Settings").grid(column=colcenter, row=nNumRow); nNumRow += 1
+    
+    radio = ttk.Checkbutton(frm,text="Nouveau?",onvalue=1,state=1)
+    radio.grid(column=colcenter, row=nNumRow); nNumRow += 1
+    print(radio.configure().keys())
+    
+    # liste
+    def get_video_name():
+        showinfo("Alerte", vidlist.get())
+
+    vidlist = Listbox(root)
+    vidlist.insert(1, "Python")
+    vidlist.insert(2, "PHP")
+    vidlist.insert(3, "jQuery")
+    vidlist.insert(4, "CSS")
+    vidlist.insert(5, "Javascript")
+    vidlist.activate(2) # set focus to a specific line (seems not to work)
+    vidlist.grid(column=1, row=nNumRow); nNumRow += 10
+    print("sel: " + str(vidlist.curselection() ))
+        
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=colcenter, row=nNumRow)
     root.mainloop()
     
     

f6e749637f645fb536346b0bae7b84cc1525418a
Alexander Mazel <amazel@aldebaran.com>
2024-06-28 21:11:33 +0200

    lecture bien synchro

--- rpiviewer/rpiviewer.py
+++ rpiviewer/rpiviewer.py
@@ -3,6 +3,8 @@
 from tkinter import *
 from tkinter import ttk
 import cv2
+import os
+import time
 
 # idée: faire un rsync avec le serveur et proposé de lire des vidéos depuis le disque local ?
 # on choisit en local la vidéo
@@ -55,43 +57,104 @@ def sync_remote_video():
 	"""
 	pass
 
-def show_video_fullscreen( filename ):
-		
-	print("INF: show_video_fullscreen: loading '%s'" % filename )
-	import cv2
-	import numpy as np
-	 
-	# Create a VideoCapture object and read from input file
-	# If the input is the camera, pass 0 instead of the video file name
-	
-	print("opening...")
-	cap = cv2.VideoCapture(filename)
-	 
-	# Check if camera opened successfully
-	if (cap.isOpened()== False): 
-	  print("Error opening video stream or file")
-	 
-	# Read until video is completed
-	while(cap.isOpened()):
-		# Capture frame-by-frame
-		ret, frame = cap.read()
-		if ret == True:
 
-			print("displaying...")
-			# Display the resulting frame
-			cv2.imshow('Frame',frame)
+def ensureFps( timeStart,nNumFrame,rWantedFps ):
+    """
+    wait a bit to ensure a fps is keeped (the loop need to be faster as we can't wait a negative time)
+    """
+    
+    rTheoricTime = nNumFrame / rWantedFps
+    rDiff = rTheoricTime - (time.time()-timeStart)
+    #~ print( "DBG: ensureFps: rDiff: %5.3fs" % rDiff )
+    rMargin = 0.01
+    if rDiff > rMargin:
+        time.sleep(rDiff-rMargin)
+    
 
-			# Press Q on keyboard to  exit
-			if cv2.waitKey(25) & 0xFF == ord('q'):
-				break
+def show_video_fullscreen( filename, bLoop = False ):
+        
+    print("INF: show_video_fullscreen: loading '%s'" % filename )
+    import cv2
+    import numpy as np
+     
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
 
-			# Break the loop
-			else: 
-				break
-	# When everything done, release the video capture object
-	cap.release()
-	
+    print( "INF: show_video_fullscreen: opening...")
+    cap = cv2.VideoCapture(filename)
+     
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+      print("ERR: show_video_fullscreen: Error opening video stream or file")
+      
+    nNbrFrame = int(cap.get(cv2. CAP_PROP_FRAME_COUNT))
+    rWantedFps = int(cap.get(cv2. CAP_PROP_FPS))
+    nDurationFrame = int(1000./rWantedFps)
+    
+    print( "DBG: show_video_fullscreen: rWantedFps: %5.3f, rDurationFrame: %5.3f" % (rWantedFps,nDurationFrame) )
+    
+    
+    strWinName = "Frame"
+    
+    cv2.namedWindow(strWinName, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(strWinName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    
+    nNumFrame = 0
+    timeStart = time.time()
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret != True:
+            if not bLoop:
+                break
+            else:
+                # restart at begin
+                cap.release()
+                cap = cv2.VideoCapture(filename)
+                ret, frame = cap.read()
+                nNumFrame = 0
+                timeStart = time.time()
+                
+            
+        if 1:
+            cv2.putText(frame,"%d/%d, t:%5.2fs" % (nNumFrame,nNbrFrame,time.time()-timeStart), 
+                (10,30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                1,
+                (0x7F, 0x7F, 0x7F),
+                1,
+                2)
+
+        # Display the resulting frame
+        cv2.imshow(strWinName,frame)
+
+        # Press Q on keyboard to  exit
+        ch = cv2.waitKey(nDurationFrame//2) & 0xFF # le //2 is to ensure we're faster, then we'll wait a bit in ensureFps
+        if  ch == ord('q') or ch == 27:
+            print( "INF: show_video_fullscreen: quit received")
+            break
+            
+                
+        nNumFrame += 1
+            
+        ensureFps(timeStart,nNumFrame,rWantedFps)
+            
+        if (nNumFrame % 100) == 0:
+            print("DBG: real fps: %5.2f" % (nNumFrame/(time.time()-timeStart)) )
+            
+
+
+
+    # When everything done, release the video capture object
+    cap.release()
+
 	
 #~ show_user_settings()
 
-show_video_fullscreen("/home/na/videos/ads_manpower.mp4")
+strLocalPath = "/home/na"
+
+if os.name == "nt":
+    strLocalPath = "c:/"
+
+show_video_fullscreen("/videos/sdaec_farmcow.mp4", bLoop=True)

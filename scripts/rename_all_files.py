8d9a5b4f1f97086a514ea4487ae6fd62cbc95a43
Alexander Mazel <amazel@aldebaran.com>
2024-10-07 22:18:11 +0200

    script pour fairmont

---
+++ scripts/rename_all_files.py
@@ -0,0 +1,21 @@
+
+
+import sys
+
+def rename_all_files_in_folder( strPath, strBefore, strAfter ):
+    files = os.listdir(strPath)
+    cpt = 0
+    for f in files:
+        absf = strPath+f
+        newf = strPath+f.replace(strBefore, strAfter)
+        if absf == newf:
+            continue
+        print("INF: rename_all_files_in_folder: %s => %s" % () )
+        if 0:
+            os.rename(absf,newf)
+        cpt += 1
+    print("INF: rename_all_files_in_folder: %s file(s) renammed" % cpt )
+    
+    
+    
+rename_all_files_in_folder("./", "__2024_10_07__from_fashion", "__2024_10_07__from_fairmont")

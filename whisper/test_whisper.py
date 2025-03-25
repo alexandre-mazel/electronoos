d9685d0998a0c1025d78850840712666dda4d2f1
Alexander Mazel <amazel@aldebaran.com>
2025-03-25 15:26:32 +0100

    up

--- whisper/test_whisper.py
+++ whisper/test_whisper.py
@@ -1,5 +1,8 @@
 import whisper # pip install openai-whisper
 import os
+import time
+
+time_begin = time.time()
 
 if os.name == "nt":
     model = whisper.load_model("base")
@@ -30,14 +33,18 @@ seen in C:\\Python39\\Lib\\site-packages\\whisper\\audio.py:
     except CalledProcessError as e:
         raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
 """
+duration = time.time() - time_begin
+print("model loaded in %.2fs" % duration ) # mstab7 base: 0.85s, azure large: 
 
-print("model loaded...")
-
+time_begin = time.time()
 #~ result = model.transcribe("on_the_fly_mehdi.wav")
-result = model.transcribe("on_the_fly_mehdi.wav", language="fr", fp16=False, verbose=True)
+result = model.transcribe("on_the_fly_mehdi.wav", fp16=False, verbose=True) # , language="fr",
+
+duration = time.time() - time_begin
+print("transcribed in %.2fs" % duration )
 
 print(f'The text in audio: \n {result["text"]}')
-print(f'Lang detected: \n {result["language"]}')
+print(f'Lang detected: {result["language"]}')
 #~ print(dir(result))
 #~ print(result)
 #~ print(result.avg_logprob)
@@ -46,3 +53,41 @@ print("Detail:")
 for s in result["segments"]:
     print( "[%s --> %s]: %s" % (s["start"], s["end"], s["text"]) )
     print( "  temp: %.2f, avg_logprob: %.2f, compression_ratio: %.2f, no_speech_prob: %.2f\n" % (s["temperature"], s["avg_logprob"], s["compression_ratio"], s["no_speech_prob"] ) )
+    
+    
+"""
+Result model base on mstab7:
+
+Lang detected: fr
+Detail:
+[0.0 --> 6.0]:  Il dit à mailleur, on voit, bah ça doit être pas mal, bah voilà ça doit être pareil.
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[7.0 --> 10.0]:  Surtout que c'est fatagé, ça appartient là.
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[10.0 --> 13.0]:  Il a y compris un mot de texte d'un homme politique.
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[15.0 --> 17.0]:  C'est ça faire la née des entretiens.
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[18.0 --> 20.0]:  Est-ce que c'est clair pour tout le monde, ça ?
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[22.0 --> 23.0]:  Ouais, ou pas ?
+  temp: 0.00, avg_logprob: -0.61, compression_ratio: 1.47, no_speech_prob: 0.32
+
+[24.0 --> 34.0]:  Ok, donc, pas un zimé, il faut en transcrire à la main, pas à la main, si c'est pas à la main, c'est en local.
+  temp: 0.00, avg_logprob: -0.25, compression_ratio: 1.63, no_speech_prob: 0.07
+
+[35.0 --> 40.0]:  Ensuite, il faut bien relire faire un zimé, découper le texte en petit goût.
+  temp: 0.00, avg_logprob: -0.25, compression_ratio: 1.63, no_speech_prob: 0.07
+
+[41.0 --> 50.0]:  Et ensuite, il faut présenter les discours des uns et des autres sur chacun des thèmes que vous avez découpé.
+  temp: 0.00, avg_logprob: -0.25, compression_ratio: 1.63, no_speech_prob: 0.07
+
+[50.0 --> 55.0]:  Normalement, c'est les thèmes de la dame d'entretiens, c'est les mêmes, sur qui ça arrive pas.
+  temp: 0.00, avg_logprob: -0.38, compression_ratio: 1.11, no_speech_prob: 0.11
+
+"""

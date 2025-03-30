747ef61cf34c0a6553ae72cb324e36fd9e9708c6
Alexander Mazel <amazel@aldebaran.com>
2025-03-30 22:52:57 +0200

    working solution avec nice debug

--- arduino_prj/misbkit_dynamixel/misbkit_dynamixel.ino
+++ arduino_prj/misbkit_dynamixel/misbkit_dynamixel.ino
@@ -1,15 +1,23 @@
 /* 
-  Copyright 2020, Nikolas Lamb. 
+  Copyright 2025, Alexandre Mazel. 
 
-  scan_dynamixel_and_flash
-    This is a great program (mostly) by ROBOTIS to help find out if your servos  
-    are connected properly. Unfortunately, if you're using an UNO it doesn't 
-    work because the Dynamixels disrupt serial communication. 
-    
-    This program circumvents this problem by flashing leds on dynamixels that
-    are successfully found by the shield. Just a quick way to check if 
-    everything is properly connected. 
+
 */
+
+/*
+
+Current "normal" warning: 
+
+WARNING: library LiquidCrystal I2C claims to run on avr architecture(s) and may be incompatible with your current board which runs on esp32 architecture(s).
+
+C:\Users\alexa\dev\git\electronoos\arduino_prj\misbkit_dynamixel\misbkit_dynamixel.ino:34: warning: "DXL_DIR_PIN" redefined
+ #define DXL_DIR_PIN 4
+ 
+In file included from C:\Users\alexa\dev\git\electronoos\arduino_prj\misbkit_dynamixel\misbkit_dynamixel.ino:13:
+c:\Users\alexa\Documents\Arduino\libraries\DynamixelShield\src/DynamixelShield.h:41: note: this is the location of the previous definition
+   #define DXL_DIR_PIN  2
+*/
+
 #include <DynamixelShield.h>
 
 #if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
@@ -47,6 +55,35 @@ bool i2CAddrTest(uint8_t addr)
     }
     return false;
 }
+
+// print one message per line then warp (if int_val is set, it will also add this value)
+int lcd_print_message_next_message = 0;
+void lcd_print_message( const char * msg, int int_val = -421 )
+{
+  int nNbrPrinted = 0;
+
+  pLcd->setCursor( 0,lcd_print_message_next_message );
+  nNbrPrinted = pLcd->print( msg );
+
+  
+  if( int_val != -421 )
+  {
+    nNbrPrinted += pLcd->print( int_val );
+  }
+
+  // erase end of line:
+  while( nNbrPrinted < 20 )
+  {
+    nNbrPrinted += pLcd->print( " " );
+  }
+
+  ++lcd_print_message_next_message;
+  if( lcd_print_message_next_message > 3 )
+  {
+    lcd_print_message_next_message = 0;
+  }
+}
+
 void setup_lcd( const char * init_msg = NULL )
 {
   uint8_t i2cAddr = 0x3F;
@@ -60,23 +97,23 @@ void setup_lcd( const char * init_msg = NULL )
   pLcd = new LiquidCrystal_I2C( i2cAddr, 20, 4 ); // 0x3F sur la version de base, 0x27 sur la version oversized
   pLcd->init();                // initialize the lcd
   pLcd->backlight();           // Turn on backlight // sans eclairage on voit rien...
-  pLcd->setCursor( 0, 0 );
   if( init_msg != NULL )
   {
-    pLcd->print( init_msg );
+    lcd_print_message( init_msg );
   }
-  pLcd->print( "setup_lcd finished" );
+  lcd_print_message( "setup_lcd finished " );
 }
 
 
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 
 // default value, will be changed by last found one during the scan
-int nMotorId      = 1;
-int nNumProtocol  = 1;
-int32_t nBaudRate = 57600;
-
-
+int nMotorId      = -1;
+int nNumProtocol  = 1;      // 1
+int32_t nBaudRate = 57600;  // 57600
+#define NBR_MOTOR_MAX 16
+int anFoundMotorIds[NBR_MOTOR_MAX];
+int nNbrFoundMotor = 0;
 
 
 void setup() {
@@ -88,13 +125,22 @@ void setup() {
   DEBUG_SERIAL.begin(115200);   //set debugging port baudrate to 115200bps
   while(!DEBUG_SERIAL);         //Wait until the serial port is opened
 
-  DEBUG_SERIAL.println( "misbkit_dynamix v0.61" );
+  const char str_version[] = "misbkit_dynami v0.63"; // limit to 20 chars to print on the lcd
+
+  DEBUG_SERIAL.println( str_version );
 
   DEBUG_SERIAL.println( "INF: Setup starting..." );
 
-  setup_lcd( "misbkit_dynamixel.ino v0.61" );
+  setup_lcd( str_version );
 
+  pLcd->setCursor( 0, 2 );
   DEBUG_SERIAL.println( "INF: Scanning..." );
+
+  lcd_print_message( "Scanning..." );
+  
+  //lcd_print_message( "Testing lcd on 4 lines" );
+  //lcd_print_message( "blablabla" );
+  //lcd_print_message( "blablabla2" );
 
   DEBUG_SERIAL.print( "Serial: " );
   DEBUG_SERIAL.println( Serial );
@@ -109,7 +155,9 @@ void setup() {
   DEBUG_SERIAL.print("DXL_DIR_PIN: ");
   DEBUG_SERIAL.println(DXL_DIR_PIN);
   
-  for(int8_t protocol = 1; protocol < 3; protocol++) {
+  //for( int8_t protocol = 1; protocol < 3; ++ protocol ) 
+  for( int8_t protocol = 1; protocol < 2; ++protocol ) // increase search 
+  {
     // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
 
     dxl.setPortProtocolVersion((float)protocol);
@@ -120,13 +168,14 @@ void setup() {
       // Set Port baudrate.
       DEBUG_SERIAL.print("SCAN BAUDRATE ");
       DEBUG_SERIAL.println(baud[index]);
+      lcd_print_message( "Testing BR: ", baud[index] );
       dxl.begin(baud[index]);
       for(int id = 0; id < DXL_BROADCAST_ID; id++) 
       {
         // iterate until all ID in each baudrate is scanned.
         // DEBUG_SERIAL.println(id);
-        if(dxl.ping(id)) {
-
+        if(dxl.ping(id)) 
+        {
           dxl.torqueOff(id);
           dxl.setOperatingMode(id, OP_POSITION);
           dxl.setOperatingMode(id, OP_VELOCITY);
@@ -149,56 +198,71 @@ void setup() {
           DEBUG_SERIAL.print(", Model Number: ");
           DEBUG_SERIAL.println(dxl.getModelNumber(id));
 
+          lcd_print_message( "Found motor id: ", id );
+
           nNumProtocol = protocol;
           nBaudRate = baud[index];
-          nMotorId = id;
-          
-          found_dynamixel++;
-        }
+
+          anFoundMotorIds[nNbrFoundMotor] = id;
+          ++nNbrFoundMotor;
+        } // dxl.ping
       }
     }
   }
   
-  DEBUG_SERIAL.print("Total ");
-  DEBUG_SERIAL.print(found_dynamixel);
-  DEBUG_SERIAL.println(" DYNAMIXEL(s) found!");
+  DEBUG_SERIAL.print( "Total " );
+  DEBUG_SERIAL.print( found_dynamixel );
+  DEBUG_SERIAL.println( " DYNAMIXEL(s) found!" );
+
+  lcd_print_message( "Nbr Motor found: ", nNbrFoundMotor );
+  delay( 2000 ); // time to read message
 
 
   // set found one
-  DEBUG_SERIAL.println( "INF: Will now use:" );
-  DEBUG_SERIAL.print( "INF: protocol: " ); DEBUG_SERIAL.println( nNumProtocol );
-  DEBUG_SERIAL.print( "INF: baudrate: " ); DEBUG_SERIAL.println( nBaudRate );
-  DEBUG_SERIAL.print( "INF: nMotorId: " ); DEBUG_SERIAL.println( nMotorId );
+  // DEBUG_SERIAL.println( "INF: Will now use:" );
+  // DEBUG_SERIAL.print( "INF: protocol: " ); DEBUG_SERIAL.println( nNumProtocol );
+  // DEBUG_SERIAL.print( "INF: baudrate: " ); DEBUG_SERIAL.println( nBaudRate );
+  // DEBUG_SERIAL.print( "INF: nMotorId: " ); DEBUG_SERIAL.println( nMotorId );
 
   dxl.setPortProtocolVersion((float)nNumProtocol);
   dxl.begin(nBaudRate);
 
-  DEBUG_SERIAL.println("INF: setup finished");
+  DEBUG_SERIAL.println( "INF: setup finished" );
   
 }
 
 void loop() 
 {
-  debug_on_lcd();
-  if(1)
+  for( int nNumMotor = 0; nNumMotor < nNbrFoundMotor; ++nNumMotor )
   {
-    // fast ping pong
-    dxl.torqueOff(nMotorId);
-    dxl.setOperatingMode(nMotorId, OP_VELOCITY);
-    dxl.torqueOn(nMotorId);
-    dxl.setGoalVelocity(nMotorId, 100, UNIT_PERCENT);
-    delay(2000);
-    //dxl.setGoalVelocity(nMotorId, -10, UNIT_PERCENT); // WRN: bug in the lib for protocol 1 wirh negative values
-    dxl.setGoalVelocity(nMotorId, 1023, UNIT_RAW); // 50: very slow reverse, 1023: fast reverse, 1074: slow straight, 2047: fast straight
-    delay(2000);
-    //dxl.setGoalVelocity(nMotorId, 0, UNIT_PERCENT);
-    //delay(2000);
-    dxl.torqueOff(nMotorId);
+    nMotorId = anFoundMotorIds[nNumMotor];
+    debug_on_lcd();
+    if(1)
+    {
+      // fast ping pong
+      dxl.torqueOff(nMotorId);
+      dxl.setOperatingMode(nMotorId, OP_VELOCITY);
+      dxl.torqueOn(nMotorId);
+      dxl.setGoalVelocity(nMotorId, 100, UNIT_PERCENT);
+      delay(2000);
+      //dxl.setGoalVelocity(nMotorId, -10, UNIT_PERCENT); // WRN: bug in the lib for protocol 1 wirh negative values
+      dxl.setGoalVelocity(nMotorId, 1023, UNIT_RAW); // 50: very slow reverse, 1023: fast reverse, 1074: slow straight, 2047: fast straight
+      delay(2000);
+      //dxl.setGoalVelocity(nMotorId, 0, UNIT_PERCENT);
+      //delay(2000);
+      dxl.torqueOff(nMotorId);
+    }
   }
 }
 
 
 void debug_on_lcd(void)
 {
-  
+  pLcd->clear();
+  pLcd->setCursor( 0, 0 );
+  pLcd->print( "Protocol: " ); pLcd->print( nNumProtocol );
+  pLcd->setCursor( 0, 1 );
+  pLcd->print( "BaudRate: " ); pLcd->print( nBaudRate );
+  pLcd->setCursor( 0, 2 );
+  pLcd->print( "MotorId: " ); pLcd->print( nMotorId );
 }

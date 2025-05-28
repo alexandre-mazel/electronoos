afae1e1c1b23a2edbe47194e5c3af3d38a279f23
Alexandre Mazel RPI <alexandre.zelma+rpitv@gmail.com>
2025-05-28 18:01:35 +0200

    up

--- raspberry/microphone.py
+++ raspberry/microphone.py
@@ -4,6 +4,7 @@ import pyaudio  # sudo apt install python3-pyaudio
 
 
 def listAudioInDevices():
+    print("DBG: listAudioInDevices: starting...")
     p = pyaudio.PyAudio()
     info = p.get_host_api_info_by_index(0)
     numdevices = info.get('deviceCount')
@@ -11,6 +12,8 @@ def listAudioInDevices():
     for i in range(0, numdevices):
         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
             print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+            
+    exit(1)
 
 
 def start_stream(callback):

6a354e19c79ffa1bf28ec7b89b236582867a5d82
Alexander Mazel <amazel@aldebaran.com>
2025-08-23 12:50:14 +0200

    ca fonctionne!

--- wiz_bulbs/midi_control.py
+++ wiz_bulbs/midi_control.py
@@ -5,6 +5,7 @@ from pywizlight import wizlight, PilotBuilder, discovery # pip install pywizligh
 import mido # pip install mido (eg 1.3.3)
 import rtmidi # pip install python-rtmidi (required by mido)
 
+import threading
 import time
 
 def print_msg( msg ):
@@ -30,37 +31,56 @@ r = 127
 g = 127
 b = 127
 bright = 255
-modified = 1
+modified = 0
 time_change = time.time()
+
+bulbs = []
 
 async def update_lights():
     
+    global modified, bright, time_change, r, g, b, bulbs
+    
+    time_last_send = time.time()
+    
+    print( "INF: update_lights: starting...")
+    
+    ips_bulb = ["192.168.0.110","192.168.0.111","192.168.0.112"]
+    
+    for ip in ips_bulb:
+        print( "INF: update_lights: connecting to %s ..." % ip )
+        bulbs.append(wizlight(ip))
+        
+    
     while 1:
-        if modified and time.time() - time_change > 0.1:
+    #~ if 1:
+        print( "INF: update_lights: modified:", modified)
+        if modified and (time.time() - time_change > 0.1 or time.time() - time_last_send > 0.5):
             modified = 0
-            print("sending...")
-            await bulbs[0].turn_on(PilotBuilder(brightness = bright,rgb = (r, g, b)))
-        time.sleep(0.1)
+            time_last_send = time.time()
+            print("sending, r: %s, g: %s, b: %s" % (r,g,b))
+            # b  = bulbs[0]
+            for bulb in bulbs:
+                await bulb.turn_on(PilotBuilder(brightness = bright,rgb = (r, g, b)))
+        #~ time.sleep(0.1)
+        await asyncio.sleep(0.1)
+        
+def update_lights_thread():
+    update_lights()
     
         
 async def midi_control():
     
-    ips_bulb = ["192.168.0.110","192.168.0.111","192.168.0.112"]
+    global modified, bright, time_change, r, g, b, bulbs
     
-    bulbs = []
-    
-    for ip in ips_bulb:
-        print( "INF: midi_control: connecting to %s ..." % ip )
-        bulbs.append(wizlight(ip))
-        
     if 0:
         # check them
         for b in bulbs:
             print("turn on")
-            await b.turn_on(PilotBuilder(brightness = 255,rgb = (255, 255, 255)))
+            #~ await b.turn_on(PilotBuilder(brightness = 255,rgb = (255, 255, 255)))
+            b.turn_on(PilotBuilder(brightness = 255,rgb = (255, 255, 255)))
             time.sleep(1)
             print("turn off")
-            await b.turn_off()
+            b.turn_off()
 
     with mido.open_input() as inport:
         for msg in inport:
@@ -83,5 +103,34 @@ async def midi_control():
             
         print("loop2...") # never pass...
         
-loop = asyncio.get_event_loop()
-loop.run_until_complete(midi_control())
+#~ thread_send = threading.Thread( target = update_lights_thread )
+
+#~ thread_send.start()
+#~ midi_control()
+
+
+#~ loop = asyncio.get_event_loop()
+
+#~ asyncio.run( update_lights() )
+
+#~ loop.run_until_complete(midi_control())
+
+#~ async def main():
+    #~ # Run both tasks concurrently in the same loop
+    #~ asyncio.gather(update_lights(), midi_control())
+
+#~ asyncio.run(main())
+
+def run_loop(coro):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(coro)
+
+
+# Start each loop in its own thread
+threading.Thread(target=run_loop, args=(update_lights(),), daemon=True).start()
+threading.Thread(target=run_loop, args=(midi_control(),), daemon=True).start()
+
+# Keep main thread alive
+asyncio.get_event_loop().run_forever()
+

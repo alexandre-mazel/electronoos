c4bfedf1702cd1af02bbabe5897fd0592d178515
Alexandre Mazel RPI <alexandre.zelma+rpitv@gmail.com>
2025-10-03 00:38:48 +0200

    changes at ensad

--- raspberry/rpi_leds.py
+++ raspberry/rpi_leds.py
@@ -12,7 +12,7 @@ from rpi_ws281x import *
 import argparse
 
 # LED strip configuration:
-LED_COUNT      = 60     # Number of LED pixels.
+LED_COUNT      = 16     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -22,6 +22,16 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
+LED_COUNT      = 383 * 3# 383 par barres ?
+
+
+# Define functions which animate LEDs in various ways.
+def colorFull(strip, color):
+    """Wipe color across display a pixel at a time."""
+    print("INF: colorFull: %x" % color )
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
@@ -99,6 +109,14 @@ if __name__ == '__main__':
     try:
 
         while True:
+            
+            #~ for i in range(4 ):
+            while 1:
+                colorFull( strip, 0xFFFFFF )
+                time.sleep(1)
+                colorFull( strip, 0x0 )
+                time.sleep(1)
+                
             print ('Color wipe animations.')
             colorWipe(strip, Color(255, 0, 0))  # Red wipe
             colorWipe(strip, Color(0, 255, 0))  # Blue wipe

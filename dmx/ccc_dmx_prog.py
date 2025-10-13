13f945980ec454e65defe91304dc8fce3a348073
Alexander Mazel <amazel@aldebaran.com>
2025-10-14 00:51:03 +0200

    working on fossilation

--- dmx/ccc_dmx_prog.py
+++ dmx/ccc_dmx_prog.py
@@ -1498,7 +1498,7 @@ def getTimeStamp():
     return strTimeStamp
 
 def log( msg ):
-    fn = "/home/na/logs/ccc_dmx_prog.log"
+    fn = "/home/pi/logs/ccc_dmx_prog.log"
     fn = open(fn,"at")
     fn.write( getTimeStamp() + ": " + msg + "\n" )
     fn.close()

ee0e7a324235cce2fd220626ec0eeb2d11d890dc
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-05-05 22:28:17 +0200

    up

--- chatola/chatola_serveur.py
+++ chatola/chatola_serveur.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 @app.route("/data", methods=["POST"])
 def receive():
     data = request.json
-    print("Reçu :", data)
+    print("Recu :", data)
     return {"status": "ok"}
+
+certname = "azure."
+keyfn = "/etc/letsencrypt/live/%sobo-world.com/privkey.pem" % certname
+certfn = "/etc/letsencrypt/live/%sobo-world.com/cert.pem" % certname
 
 app.run(
     host="0.0.0.0",
     port=10000,
-    ssl_context=("/chemin/vers/fullchain.pem", "/chemin/vers/privkey.pem")
+    ssl_context=(certfn,keyfn)
 )

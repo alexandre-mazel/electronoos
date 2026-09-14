aa576d9b02e49dd2ba24a80fd69dbd840378b2a4
Alexandre Mazel Champ <alexandre.zelma@gmail.com>
2026-09-14 17:27:01 +0200

    reglage bof

--- chatola/chatola_tchat.py
+++ chatola/chatola_tchat.py
@@ -120,7 +120,7 @@ strModel = "qwen3:8B" # 2s 6.2GB 100%gpu, rapide car pas de thinking, par contre
 #~ strModel = "mistral-small3.1:24b" # autour de 30sec, modele de 16GB a 58% en cpu
 #~ strModel = "ministral-3:14b" # 3-4s, 10GB, 34%cpu, mais ne respecte pas les consignes de pas mettre d'etoile, il en met a chaque fois.
 #~ strModel = "ministral-3:14b-instruct" # pas le bon nom
-strModel = "ministral-3:14b-instruct-2512-q4_K_M" # 10GB 34% aussi ne semble rien changer.
+#~ strModel = "ministral-3:14b-instruct-2512-q4_K_M" # 10GB 34% aussi ne semble rien changer.
 
 #~ strModel = "gemma3:4B" # pas trop long mais des merdouilette
 #~ strModel = "gemma3:12B" # 3-4s 9GB 25%cpu, plus long que qwen3:8B mais de bons résultats

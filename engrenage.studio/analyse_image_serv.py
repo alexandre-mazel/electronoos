0f73770a8a016d4f143e8069bfe5b716ce3c7398
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-17 14:00:29 +0200

    up

--- engrenage.studio/analyse_image_serv.py
+++ engrenage.studio/analyse_image_serv.py
@@ -97,6 +97,7 @@ CORRECT : "Gaia est assise à une table avec des livres."
 
         desc = workface_tools.describe_faces_position()
         print( desc )
+        extra_instruction = desc
         
         #~ extra_instruction  = extra_instruction_hardcoded
         print( "DBG: extract_infos_from_img: extra_instruction: %s" % extra_instruction )

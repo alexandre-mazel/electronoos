f0ae2feeb67ccc9f793a8ba0153372dfcfe11815
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 09:47:24 +0200

    up almapad: ip

--- engrenage.studio/apad_ws.py
+++ engrenage.studio/apad_ws.py
@@ -18,8 +18,11 @@ def retrieve_info_on_ip( ip ):
 
     print("INF: retrieve_info_on_ip: ip:", str(data) )
 
-    data = requests.get( f"https://ipinfo.io/{ip}/json", timeout=3 ).json()
-
+    received_data = requests.get( f"https://ipinfo.io/{ip}/json", timeout=3 )
+    print("INF: retrieve_info_on_ip: received_data:", str(received_data) )
+    
+    data = received_data.json()
+    
     print("INF: retrieve_info_on_ip: data:", str(data) )
     
     org = data.get("org", "").lower()

5282224434436540f56a1b7a969fb5ba4deb97c6
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 18:40:48 +0200

    up almapad: passage a des pads jusqua 8

--- engrenage.studio/alma_markdown.js
+++ engrenage.studio/alma_markdown.js
@@ -126,7 +126,7 @@ function inlineMarkdown(text) {
 
     text = text.replace(
         /\{check\}/g,
-        '<span class="md-check">☐</span>' // was ☐ mais fonctionne pas sur samsung (mais \25A2 non plus, alors que ca fonctionne dans le css...)
+        '<span class="md-check">▢</span>' // was ☐ mais sur samsung affiche une urne, \25A2 non plus, alors que ca fonctionne dans le css..., alors je tente: ▢
     );
 
 

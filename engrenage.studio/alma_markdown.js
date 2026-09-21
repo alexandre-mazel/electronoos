bd74a8c0f2530f8ec5a7e1f373d1cd372235173f
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 09:08:16 +0200

    up alma pad

--- engrenage.studio/alma_markdown.js
+++ engrenage.studio/alma_markdown.js
@@ -460,11 +460,8 @@ function hello() {
 
 ![Une jolie image](https://example.com/image.jpg)
 
-Et même ceci :
+Et même du script dans des balises script (mais je le met pas car ca plante mon jsminifieur):
 
-<script>
-    alert("CE CODE NE S'EXECUTERA PAS");
-</script>
 `;
 
 function load_markdown( div_id, markdown_content =  markdown_example )

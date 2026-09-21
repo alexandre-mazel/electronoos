dc580a78a0f024cdafd45a04ae802b0e204fc8d4
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 09:45:27 +0200

    up almapad: ip

--- engrenage.studio/apad_ws.py
+++ engrenage.studio/apad_ws.py
@@ -40,7 +40,7 @@ def retrieve_info_on_ip( ip ):
 def get_info_on_ip( ip ):
     global info_ip
     if ip in info_ip: return info_ip[ip]
-    info = retrieve_info_on_ip()
+    info = retrieve_info_on_ip( ip )
     info_ip[ip] = info
     
     

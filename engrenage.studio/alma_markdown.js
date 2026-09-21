00101cfcc5b65141de7e7aabd84a5dd5e0b6038c
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 23:02:58 +0200

    up almapad: passage a des pads jusqua 8

--- engrenage.studio/alma_markdown.js
+++ engrenage.studio/alma_markdown.js
@@ -39,9 +39,10 @@ Don't handle:
 
 function isSamsungBrowser()
 {    
-    const ua = navigator.userAgent;
-    const bIsSamsungBrowser = /SamsungBrowser/i.test(ua) && /Android/i.test(ua);
-    return bIsSamsungBrowser
+    return false;
+    //const ua = navigator.userAgent;
+    //const bIsSamsungBrowser = /SamsungBrowser/i.test(ua) && /Android/i.test(ua);
+    //return bIsSamsungBrowser
 }
 
 function escapeHtml(text) {
@@ -134,7 +135,7 @@ function inlineMarkdown(text) {
     let replace_check = "☐";
     if( isSamsungBrowser() )
     {
-        //replace_check = "▢"; // was ☐ mais sur samsung affiche une urne, \25A2 non plus, alors que ca fonctionne dans le css..., alors je tente: ▢
+        replace_check = "▢"; // was ☐ mais sur samsung affiche une urne, \25A2 non plus, alors que ca fonctionne dans le css..., alors je tente: ▢
     }
     
     text = text.replace( /\{check\}/g, '<span class="md-check">'+replace_check+'</span>' );

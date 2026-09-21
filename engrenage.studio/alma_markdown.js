1d17367bc2489a9e17128229cc4041b9569edfc5
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 18:37:16 +0200

    up almapad: passage a des pads jusqua 8

--- engrenage.studio/alma_markdown.js
+++ engrenage.studio/alma_markdown.js
@@ -126,7 +126,7 @@ function inlineMarkdown(text) {
 
     text = text.replace(
         /\{check\}/g,
-        '<span class="md-check">\25A2</span>' // was ☐
+        '<span class="md-check">☐</span>' // was ☐ mais fonctionne pas sur samsung (mais \25A2 non plus...)
     );
 
 

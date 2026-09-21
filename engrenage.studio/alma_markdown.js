b3175df3b39f23a030c797ca43b31dd402f853fb
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-21 18:04:51 +0200

    up almapad: passage a des pads jusqua 8

--- engrenage.studio/alma_markdown.js
+++ engrenage.studio/alma_markdown.js
@@ -476,7 +476,7 @@ function parseMarkdown(markdown, toggle_check_name = "toggle_check" )
                         puceopen = "<li class='li_checked'"
                     }
                     //~ puceopen += " onclick="+toggle_check_name+"(" + nbr_clickable_area + ")>"
-                    puceopen += " data-check-index=\"" + nbr_clickable_area + "\">" // essaye de faire fonctionner sur samsung mais ca fait rien de bien!
+                    puceopen += " data-check-index=\"" + (nbr_clickable_area).toString() + "\">" // essaye de faire fonctionner sur samsung mais ca fait rien de bien!
                     nbr_clickable_area += 1;
                 }
                     

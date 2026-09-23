720adcf380266ca063d62861c478a7b51a09447d
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-23 21:25:25 +0200

    tri sur date

--- engrenage.studio/viewcloud.py
+++ engrenage.studio/viewcloud.py
@@ -231,7 +231,7 @@ def scan(nNbrMaxThumbNailToGenerate=6):
         "files": files
     }
 
-def decode_param( query )
+def decode_param( query ):
     dict_params = {
         k: v[0]
         for k, v in urllib.parseparse_qs(urllib.parse.urlparse(url).query).items()

b7cf0b356988aaaca9e86c8fc22f17704efdb3c6
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-23 22:12:40 +0200

    pwd sur viewcloud

--- engrenage.studio/viewcloud.py
+++ engrenage.studio/viewcloud.py
@@ -284,8 +284,9 @@ def index(req):
         if  "id" in dArgs and "pwd" in dArgs:
             id = dArgs["id"]
             password_test = dArgs["pwd"]
-            hashed = misctools.getEnv( id +"_pwdmd" ) #generate by hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-            hashed = bcrypt.hashpw(password_test, bcrypt.gensalt()); print( "DBG: un bon hashage pour cet id pourrait etre: '%s'" % hashed )
+            print( "DBG: index: id: '%s', password_test: '%s'" % (id,password_test) )
+            hashed = misctools.getEnv( id +"_pwdmd" ) # generate by hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+            hashed = bcrypt.hashpw( password_test, bcrypt.gensalt()); print( "DBG: un bon hashage pour cet id pourrait etre: '%s'" % hashed )
             print( "DBG: index: id: '%s', password_test: '%s', hashed: '%s'" % (id,password_test,hashed) )
             if bcrypt.checkpw(password_test, hashed):
                 return send_json(scan())

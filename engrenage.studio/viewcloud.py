4db3c603c412887728bd807ffda992b9fb89ed37
Alexandre Mazel <alexandre.zelma+fork@gmail.com>
2026-09-23 21:48:48 +0200

    pwd sur viewcloud

--- engrenage.studio/viewcloud.py
+++ engrenage.studio/viewcloud.py
@@ -247,10 +247,10 @@ def scan(nNbrMaxThumbNailToGenerate=6):
         "files": files
     }
 
-def decode_param( url ):
+def decode_param( query ):
     dict_params = {
         k: v[0]
-        for k, v in urllib.parse.parse_qs(urllib.parse.urlparse(url).query).items()
+        for k, v in urllib.parse.parse_qs(query).items()
     }
     
     return dict_params
